1164b716244d3ac7fb03edc19b081746b41c98b1
SigurdJanson <emailjs@web.de>
2021-12-28 18:04:27 +0100

    New class for single die rolls

---
+++ dev/RollLogic/DieRoll.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FateExplorer.WPA.RollLogic
+{
+    /// <summary>
+    /// Represents a single die to roll
+    /// </summary>
+    public class DieRoll
+    {
+        protected IRandomNG RNG;
+
+        /// <summary>
+        /// The number of sides for the die
+        /// </summary>
+        public int Sides { get;  protected set; }
+
+        /// <summary>
+        /// The current roll value
+        /// </summary>
+        public int OpenRoll { get; protected set; } = 0;
+
+        /// <summary>
+        /// The previous roll value
+        /// </summary>
+        public int PrevRoll { get; protected set; } = 0;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sides">The sides of the die</param>
+        public DieRoll(int sides)
+        {
+            Sides = sides;
+            RNG = new RandomMersenne();
+        }
+
+        /// <summary>
+        /// Roll the die and return the value
+        /// </summary>
+        /// <returns>The result of the die roll</returns>
+        public int Roll()
+        {
+            PrevRoll = OpenRoll;
+            OpenRoll = RNG.IRandom(1, Sides);
+            return OpenRoll;
+        }
+    }
+}
