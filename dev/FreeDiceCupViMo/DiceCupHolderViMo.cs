0789ac215bb610a217ea3081c7c696200ca4d990
SigurdJanson <emailjs@web.de>
2021-12-30 23:19:52 +0100

    Steps towards a free roll page incl. ViMo

---
+++ dev/FreeDiceCupViMo/DiceCupHolderViMo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace FateExplorer.WPA.FreeDiceCupViMo
+{
+    public class DiceCupHolderViMo
+    {
+        public DiceCupHolderViMo()
+        {
+            CupList = new();
+            CupList.Add(new DiceCupViMo("Ability", "Plain d20 rolls", new int[1] { 20 }, true));
+            CupList.Add(new DiceCupViMo("Skill", "3d20 rolls", new int[3] { 20, 20, 20 }, true));
+            CupList.Add(new DiceCupViMo("Six", "Plain d6 rolls", new int[1] { 6 }, true));
+            CupList.Add(new DiceCupViMo("Botch", "Rolls with 2d6", new int[2] { 6, 6 }, true));
+        }
+
+        public List<DiceCupViMo> CupList { get; protected set; }
+
+        public int Count { get => CupList.Count; }
+
+        public DiceCupViMo this[int i] => CupList[i];
+
+        public void AddCup(string name, string descr, int[] eyes)
+        {
+            if (eyes.Length == 0) return;
+            CupList.Add(new DiceCupViMo(name, descr, eyes));
+        }
+    }
+}
---
+++ dev/FreeDiceCupViMo/DiceCupViMo.cs
@@ -0,0 +1,76 @@
+﻿using FateExplorer.WPA.RollLogic;
+using System;
+
+namespace FateExplorer.WPA.FreeDiceCupViMo
+{
+    public enum CupType
+    {
+        Single = 1, Multi = 2, MixedMulti = 3
+    }
+
+    public class DiceCupViMo
+    {
+        public DiceCupViMo(string name, string descr, int[] eyes, bool factoryDefault = false)
+        {
+
+            Name = name ?? "";
+            Description = descr ?? "";
+            FactoryDefault = factoryDefault;
+Console.WriteLine($"{Name}: Props are done");
+
+            // Set cup type and eyes
+            if (eyes is null || eyes.Length == 0)
+                throw new ArgumentNullException(nameof(eyes), "Internal error: empty dice cup defined");
+
+            if (eyes.Length == 1)
+            {
+                Type = CupType.Single;
+                Eyes = eyes[0];
+            }
+            else
+            {
+                bool AllTheSame = true;
+                for(int i = 1; i < eyes.Length; i++)
+                    if (eyes[0] != eyes[i])
+                    {
+                        AllTheSame = false;
+                        break;
+                    }
+                Type = AllTheSame ? CupType.Multi : CupType.MixedMulti;
+                Eyes = AllTheSame ? eyes[0] : 0;
+            }
+Console.WriteLine($"{Name}: Eyes are done");
+            // Make a roller
+            switch (Type)
+            {
+                case CupType.Single: Roller = new DieRoll(Eyes); break;
+                case CupType.Multi: Roller = new MultiDieRoll(Eyes, eyes.Length); break;
+                case CupType.MixedMulti: throw new NotImplementedException(); //TODO
+            }
+Console.WriteLine($"{Name}: Roller is done");
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Is this dice cup built in by FateExplorer or user-defined?
+        /// </summary>
+        public bool FactoryDefault { get; protected set; }
+
+        /// <summary>
+        /// The eyes of the dice in this dice cup. Returns 0 if the dice are mixed.
+        /// </summary>
+        public int Eyes { get; protected set; }
+
+        public CupType Type { get; set; }
+
+        protected IRoll Roller { get; set; }
+
+        public int GetRollResult()
+        {
+            return Roller.Roll();
+        }
+    }
+}
