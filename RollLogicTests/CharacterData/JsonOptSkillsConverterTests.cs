275d4edf575cefb0c2620559e7e506b207c84933
SigurdJanson <emailjs@web.de>
2022-01-01 23:32:47 +0100

    Created new Json importer to handle the screwed up format of the optholit (json property name is abused as dictionary key).

---
+++ RollLogicTests/CharacterData/JsonOptSkillsConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using FateExplorer.CharacterData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RollLogicTests.CharacterData
+{
+    [TestFixture]
+    public class JsonOptSkillsConverterTests
+    {
+        Dictionary<string, int> TargetResult = new()
+        {
+            { "SPELL_6", 7 },
+            { "SPELL_8", 6 },
+            { "SPELL_79", 0 },
+            { "SPELL_20", 11 },
+            { "SPELL_134", 7 },
+            { "SPELL_35", 5 },
+            { "SPELL_40", 4 },
+            { "SPELL_72", 1 },
+            { "SPELL_3", 5 },
+            { "SPELL_7", 5 },
+            { "SPELL_30", 5 },
+            { "SPELL_9", 5 }
+        };
+
+        public class TestClass
+        {
+            [JsonPropertyName("beginning")]
+            public int Beginning { get; set; }
+
+            [JsonPropertyName("spells"), JsonConverter(typeof(JsonOptSkillsConverter))]
+            public Dictionary<string, int> Spells { get; set; }
+
+            [JsonPropertyName("end")]
+            public string End { get; set;}
+        }
+
+
+        [Test]
+        public void Read_StateUnderTest_ReadCorrectly()
+        {
+
+            // Arrange
+            string BasePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\TestDataFiles"));
+            string fileName = Path.GetFullPath(Path.Combine(BasePath, $"_test_spellsjsonimport.json"));
+            string jsonString = File.ReadAllText(fileName);
+
+            // Act
+            TestClass Result = JsonSerializer.Deserialize<TestClass>(jsonString);
+            // Act
+            //var result = jsonOptSkillsConverter.Read(ref reader, typeToConvert, options);
+
+            // Assert
+            Assert.AreEqual(12, Result.Spells.Count);
+            foreach (var t in TargetResult)
+                Assert.AreEqual(t.Value, Result.Spells[t.Key]);
+        }
+
+
+
+        [Test]
+        public void CanConvert_CorrectType_ReturnsTrue()
+        {
+            // Arrange
+            var jsonOptSkillsConverter = new JsonOptSkillsConverter();
+            Dictionary<string, int> TestObject = new();
+            Type objectType = TestObject.GetType(); //typeof(TestObject.Ty);
+
+            // Act
+            var Result = jsonOptSkillsConverter.CanConvert(objectType);
+
+            // Assert
+            Assert.IsTrue(Result);
+        }
+    }
+}
---
+++ dev/CharacterData/JsonOptSkillsConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FateExplorer.CharacterData
+{
+    public class JsonOptSkillsConverter : JsonConverter<Dictionary<string, int>>
+    {
+        private string[] AllowedSkillNames = new string[] { "TAL", "CT", "SPELL", "LITURGY" };
+
+        // This is used when you're converting the C# List back to a JSON format
+        public override void Write(Utf8JsonWriter writer, Dictionary<string, int> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        // This is when you're reading the JSON object and converting it to C#
+        public override Dictionary<string, int> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException();
+
+            var Result = new Dictionary<string, int>();
+
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.StartObject)
+                    continue;
+
+                if (reader.TokenType == JsonTokenType.EndObject)
+                    return Result;
+
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    // Read key
+                    string Key = reader.GetString();
+
+                    bool isSkillKey = false;
+                    foreach (var an in AllowedSkillNames)
+                        if (Key.StartsWith(an))
+                        {
+                            isSkillKey = true;
+                            break;
+                        }
+                    if (!isSkillKey)
+                        throw new JsonException("Unknow skill. Cannot read it.");
+
+                    // Read data
+                    reader.Read();
+                    if (reader.TokenType != JsonTokenType.Number)
+                        throw new JsonException("Numeric skill value was expected but not found");
+
+                    int Value = reader.GetInt32();
+                    //SkillsEntryOpt Element = new() { Id = key, Value = Value };
+
+                    Result.Add(Key, Value);
+                }
+            }
+
+            return Result;
+        }
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(Dictionary<string, int>);
+    }
+}
