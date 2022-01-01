9c13035625c7e0e645c2b335713d433c324115bc
SigurdJanson <emailjs@web.de>
2022-01-01 18:12:15 +0100

    Data service class for botches

---
+++ RollLogicTests/GameData/BotchDbTests.cs
@@ -0,0 +1,63 @@
+﻿using FateExplorer.GameData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace RollLogicTests.GameData
+{
+    [TestFixture]
+    public class BotchDBTests
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        private static BotchDB CreateBotchDB()
+        {
+            return new BotchDB();
+        }
+
+
+
+        [Test]
+        public void LoadFromFile_ParseSuccessful(
+            [Values("Attack", "Parry", "Dodge")] string Roll, 
+            [Values("Melee", "Unarmed", "Ranged", "Shield")] string Type, 
+            [Range(2, 12)] int DiceEyes,
+            [Values("de", "en")] string Language)
+        {
+            // Arrange
+            string BasePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\TestDataFiles"));
+            string fileName = Path.GetFullPath(Path.Combine(BasePath, $"botches_{Language}.json"));
+            string jsonString = File.ReadAllText(fileName);
+
+            // Act
+            BotchDB Result = JsonSerializer.Deserialize<BotchDB>(jsonString);
+
+            // Assert
+            if (Roll == "Attack" && Type == "Shield")
+                Assert.Throws<KeyNotFoundException>(() => Result.GetBotch(Roll, Type, DiceEyes));
+            else if (Roll == "Parry" && Type == "Ranged")
+                Assert.Throws<KeyNotFoundException>(() => Result.GetBotch(Roll, Type, DiceEyes));
+            else
+                Assert.NotNull(Result.GetBotch(Roll, Type, DiceEyes));
+        }
+
+        [Test]
+        public void Count_ContentNotLoaded_Return0()
+        {
+            // Arrange
+            //-BotchDB DB = CreateBotchDB();
+
+            // Act
+            //-int Count = DB.Count;
+
+            // Assert
+            //-Assert.AreEqual(0, Count);
+        }
+    }
+}
---
+++ dev/GameData/BotchDB.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FateExplorer.GameData
+{
+
+    // BotchProvider myDeserializedClass = JsonConvert.DeserializeObject<BotchProvider>(myJsonResponse); 
+
+    public class BotchDB
+    {
+        [JsonPropertyName("Effects")]
+        public IReadOnlyList<BotchEntry> Botches { get; set; }
+
+        [JsonPropertyName("Tables")]
+        public IReadOnlyList<BotchTable> Tables { get; set; }
+
+
+        public BotchTable GetBotchTable(string Roll, string Type)
+        {
+            foreach (var t in Tables)
+            {
+                if (t.Type != Type) continue;
+                foreach (var r in t.Roll)
+                    if (r == Roll)
+                        return t;
+            }
+
+            throw new KeyNotFoundException("Unknown botch table");
+        }
+
+
+        public BotchEntry GetBotch(string Roll, string Type, int DiceEyes)
+        {
+            // Find correct table in `Tables`
+            var Table = GetBotchTable(Roll, Type);
+
+            // Get reference
+            BotchEffect effect = null;
+            foreach (var i in Table.Effect)
+                foreach(var e in i.DiceEyes)
+                    if (e == DiceEyes)
+                        effect = i;
+
+            // Return BotchEffect
+            if (effect is not null)
+                foreach (var b in Botches)
+                    if (b.Id == effect.Ref)
+                        return b;
+
+            // if nothing found: error
+            throw new Exception("Unknown botch effect");
+        }
+
+
+        public string GetUrl(string Roll, string Type)
+        {
+            // Find correct table in `Tables`
+            var Table = GetBotchTable(Roll, Type);
+
+            // Return URL
+            return Table.Url;
+        }
+    }
+
+
+
+    public class BotchEntry
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set;  }
+
+        [JsonPropertyName("label")]
+        public string Label { get; set; }
+
+        [JsonPropertyName("descr")]
+        public string Descr { get; set; }
+    }
+
+
+    public class BotchEffect
+    {
+        [JsonPropertyName("dr")]
+        public int[] DiceEyes { get; set; }
+
+        [JsonPropertyName("ref")]
+        public string Ref { get; set; }
+    }
+
+
+    public class BotchTable
+    {
+        [JsonPropertyName("Roll")]
+        public string[] Roll { get; set; }
+
+        [JsonPropertyName("Type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("Effect")]
+        public IReadOnlyList<BotchEffect> Effect { get; set; }
+    }
+
+
+}
