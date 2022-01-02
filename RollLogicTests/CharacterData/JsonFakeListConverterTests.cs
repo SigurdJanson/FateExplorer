5c7362863b87520d8d891001ae79921c63961eae
SigurdJanson <emailjs@web.de>
2022-01-02 21:05:05 +0100

    Âdded a Json converter to import the "fake" lists that the Optolith app uses (esp. in belongings).

---
+++ RollLogicTests/CharacterData/JsonFakeListConverterTests.cs
@@ -0,0 +1,67 @@
+﻿using FateExplorer.CharacterData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RollLogicTests.CharacterData
+{
+    [TestFixture]
+    public class JsonFakeListConverterTests
+    {
+
+        public class TestClass
+        {
+            [JsonPropertyName("beginning")]
+            public int Beginning { get; set; }
+
+            [JsonPropertyName("items"), JsonConverter(typeof(JsonFakeListConverter<BelongingItem>))]
+            public List<BelongingItem> Items { get; set; }
+
+            [JsonPropertyName("end")]
+            public string End { get; set; }
+        }
+
+
+
+        [Test]
+        public void Write_StateUnderTest_ExpectedBehavior()
+        {
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Read_StateUnderTest_ExpectedBehavior()
+        {
+            string BasePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\TestDataFiles"));
+            string fileName = Path.GetFullPath(Path.Combine(BasePath, "_test_fakelistimport.json"));
+            string jsonString = File.ReadAllText(fileName);
+
+            // Act
+            TestClass Result = JsonSerializer.Deserialize<TestClass>(jsonString);
+
+            // Assert
+            Assert.AreEqual(3, Result.Items.Count);
+            //Assert.AreEqual(Ability1, Result[0].ShortName);
+            //Assert.AreEqual(AbilityLast, Result[^1].ShortName);
+        }
+
+        [Test]
+        public void CanConvert_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var jsonFakeListConverter = new JsonFakeListConverter<TestClass>();
+            List<TestClass> TestObject = new();
+            Type objectType = TestObject.GetType(); //typeof(TestObject.Ty);
+
+            // Act
+            var Result = jsonFakeListConverter.CanConvert(objectType);
+
+            // Assert
+            Assert.IsTrue(Result);
+        }
+    }
+}
---
+++ dev/CharacterData/JsonFakeListConverter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FateExplorer.CharacterData
+{
+    /// <summary>
+    /// Handles deserialisation of fake lists that are actually an array (which would be <c>[ ... ]</c> in json)
+    /// but instead there is a series of objects with keys like this:
+    /// <code>
+    /// "items": {
+    ///    "ITEM_1": { ... }, // object of type T
+    ///    "ITEM_2": { ... },
+    ///    ...
+    /// }
+    /// </code>
+    /// </summary>
+    /// <typeparam name="T">Object type </typeparam>
+    /// <remarks>
+    /// Inspired by https://stackoverflow.com/questions/16085805/recursively-call-jsonserializer-in-a-jsonconverter
+    /// </remarks>
+    public class JsonFakeListConverter<T> : JsonConverter<List<T>>
+    {
+        ///// <summary>
+        ///// Dummy to fool JsonSerializer into not using this converter recursively
+        ///// </summary>
+        //public T ToBeImported { get; set; }
+
+        //-private string[] AllowedNames = new string[] { "ITEM" };
+
+
+        // 
+        public override void Write(Utf8JsonWriter writer, List<T> value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public override List<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException();
+
+            var Result = new List<T>();
+
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.EndObject)
+                    return Result;
+
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    // READ KEY
+                    string Key = reader.GetString();
+                    T Imported;
+
+                    //bool isValidKey = false;
+                    //foreach (var an in AllowedNames)
+                    //    if (Key.StartsWith(an))
+                    //    {
+                    //        isValidKey = true;
+                    //        break;
+                    //    }
+                    //if (!isValidKey)
+                    //    throw new JsonException("Unknow key. Cannot read it.");
+
+                    // READ DATA
+                    // Read the whole thing into a string and then pass it to
+                    // the default deserialiser.
+                    reader.Read();
+                    if (reader.TokenType == JsonTokenType.StartObject)
+                    {
+                        string RawJson = "";
+                        //-string Dummy = "";
+                        using (var jsonDoc = JsonDocument.ParseValue(ref reader))
+                        {
+                            RawJson = jsonDoc.RootElement.GetRawText();
+                        }
+
+                        //while (reader.TokenType != JsonTokenType.EndObject)
+                        //{
+                        //    switch (reader.TokenType)
+                        //    {
+                        //        case JsonTokenType.PropertyName: RawJson += "\"" + reader.GetString() + "\":"; break;
+                        //        case JsonTokenType.String: RawJson += "\"" + reader.GetString() + "\"" + ","; break;
+                        //        case JsonTokenType.Number: RawJson += reader.GetDouble() + ","; break;
+                        //        case JsonTokenType.True:
+                        //        case JsonTokenType.False: RawJson += reader.GetBoolean().ToString().ToLower() + ","; break;
+                        //        case JsonTokenType.StartArray: RawJson += "["; break;
+                        //        case JsonTokenType.EndArray: RawJson += "]" + ","; break;
+                        //        //case JsonTokenType.EndObject: RawJson += "}"; break;
+                        //        case JsonTokenType.StartObject: RawJson += "{"; break;
+                        //    }
+                        //    reader.Read();
+                        //}
+                        //RawJson += "}"; // Let us not forget the end token
+                        Imported = JsonSerializer.Deserialize<T>(RawJson, options);
+
+                        //-T ObjectResult = JsonSerializer.Deserialize<T>(jsonString);
+                    }
+                    else
+                        throw new JsonException("Object was expected but not found");
+
+                    Result.Add(Imported);
+                }
+            }
+
+            return Result;
+        }
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(List<T>);
+
+    }
+}
