5840ef780f87ecfc14be8d18860767df71dcee82
SigurdJanson <emailjs@web.de>
2022-01-04 09:12:40 +0100

    Added the model for character skills

---
+++ dev/GameLogic/CharacterSkillM.cs
@@ -0,0 +1,65 @@
+﻿using FateExplorer.GameData;
+
+namespace FateExplorer.GameLogic
+{
+    
+
+    public class CharacterSkillM
+    {
+        public ICharacterM Hero { get; protected set; }
+
+        public CharacterSkillM(SkillDbEntryBase gameData, int value, ICharacterM character)
+        {
+            Hero = character;
+            //
+            Id = gameData.Id;
+            Name = gameData.Name;
+            ClassName = gameData.Class;
+            ClassId = gameData.ClassId;
+            Abilities = new string[3];
+            Abilities[0] = gameData.Ab1;
+            Abilities[1] = gameData.Ab2;
+            Abilities[2] = gameData.Ab3;
+            //
+            if (gameData is ArcaneSkillDbEntry Arcane)
+            {
+                ModifyAgainst = Arcane.ModAgainst;
+                Tradition = new string[1] { Arcane.Category };
+                Domain = gameData.Domain;
+            }
+            else if (gameData is KarmaSkillDbEntry Karma)
+            {
+                ModifyAgainst = Karma.ModAgainst;
+                Tradition = Karma.Tradition.Clone() as string[];
+            }
+            //
+            Value = value;
+        }
+
+        public string Id { get; protected set; }
+
+        public string Name { get; protected set; }
+
+        public string ClassName { get; protected set; }
+
+        public int ClassId { get; protected set; }
+
+        public int Value { get; protected set; }
+
+        public string[] Abilities { get; protected set; }
+
+        /// <summary>
+        /// Resilience that sets the modifier against which any rolls 
+        /// have to be tested.
+        /// </summary>
+        public string ModifyAgainst { get; protected set; } // modagainst: SK / ZK
+
+        /// <summary>
+        /// Divine or arcane tradtion of a liturgy or spell
+        /// </summary>
+        public string[] Tradition { get; protected set; } // tradition in karmaskills; Category in arcane skills
+
+        //
+        public SkillDomain Domain { get; protected set; }
+    }
+}
---
+++ dev/GameLogic/CharacterSkillsM.cs
@@ -0,0 +1,64 @@
+﻿using FateExplorer.GameData;
+using FateExplorer.CharacterData;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FateExplorer.GameLogic
+{
+
+
+    public class CharacterSkillsM
+    {
+        public ICharacterM Hero { get; protected set; }
+
+        public Dictionary<string, CharacterSkillM> Skills { get; set; }
+
+        public Dictionary<SkillDomain,bool> MasteredDomains { get; protected set; }
+
+
+        public CharacterSkillsM(ICharacterM character, ICharacterImporter import, IGameDataService gameData)
+        {
+            Hero = character;
+
+            Skills = new();
+            MasteredDomains = new();
+
+            // Basic mundane skills
+            MasteredDomains.Add(SkillDomain.Basic, true);
+            foreach (var v in import.GetTalentSkills())
+            {
+                CharacterSkillM skill = new(gameData.Skills[v.Key], v.Value, character);
+                Skills.Add(v.Key, skill);
+            }
+            // Arcane skills
+            var ToImport = import.GetArcaneSkills();
+            MasteredDomains.Add(SkillDomain.Arcane, ToImport?.Any() ?? false);
+            foreach (var v in ToImport)
+            {
+                CharacterSkillM skill = new(gameData.ArcaneSkills[v.Key], v.Value, character);
+                Skills.Add(v.Key, skill);
+            }
+
+            // Karma skills
+            ToImport = import.GetKarmaSkills();
+            MasteredDomains.Add(SkillDomain.Karma, ToImport?.Any() ?? false);
+            foreach (var v in ToImport)
+            {
+                CharacterSkillM skill = new(gameData.KarmaSkills[v.Key], v.Value, character);
+                Skills.Add(v.Key, skill);
+            }
+        }
+
+
+        public List<string> GetSkillNames(SkillDomain? Domain, string Filter)
+        {
+            List<string> names = new();
+            foreach(var s in Skills.Values)
+            {
+                if (s.Name.Contains(Filter) && s.Domain == Domain)
+                    names.Add(s.Name);
+            }
+            return names;
+        }
+    }
+}
