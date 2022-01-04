4ac8d3f55d47fe5c4629c46f625112b18eb2fcb9
SigurdJanson <emailjs@web.de>
2022-01-04 09:14:05 +0100

    Model for character abilities

---
+++ dev/GameLogic/AbilityM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace FateExplorer.GameLogic
+{
+
+
+    public class AbilityM
+    {
+        public const string COU = "ATTR_1";
+        public const string SGC = "ATTR_2";
+        public const string INT = "ATTR_3";
+        public const string CHA = "ATTR_4";
+        public const string DEX = "ATTR_5";
+        public const string AGI = "ATTR_6";
+        public const string CON = "ATTR_7";
+        public const string STR = "ATTR_8";
+
+
+        public string Id { get; protected set; }
+
+        public string Name { get; protected set; }
+
+        public string ShortName { get; protected set; }
+
+        public int Value { get; protected set; }
+
+        public AbilityM(string id, string name, string shortName, int value)
+        {
+            Id = id;
+            Name = name;
+            ShortName = ShortName;
+            Value = value;
+        }
+
+
+
+        // TODO: Effective Values are ViMo
+        public int EffectiveValue { get; protected set; }
+
+
+        public void ChangeEffectiveValue(int Modifier, Func<int, int, int> Operation = null)
+        {
+            if (Operation is null)
+                EffectiveValue += Modifier;
+            else
+                EffectiveValue = Operation(EffectiveValue, Modifier);
+
+            if (EffectiveValue < 0) EffectiveValue = 0;
+        }
+    }
+
+
+}
