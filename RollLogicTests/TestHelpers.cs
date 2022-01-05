8b781f000cbc61409fa72ae97acced6b8f6c80c6
SigurdJanson <emailjs@web.de>
2022-01-05 18:57:44 +0100

    Added some helpers to improve the deep comparison of objects

---
+++ RollLogicTests/TestHelpers.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollLogicTests
+{
+    internal static class TestHelpers
+    {
+        /// <summary>
+        /// Compares two objects on the topmost level of properties without recursive
+        /// entry into complex objects. Be careful with reference types!
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="self"></param>
+        /// <param name="to"></param>
+        /// <param name="ignore"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Formerly known as PublicInstancePropertiesEqual, 
+        /// see <see href="https://stackoverflow.com/a/844855/13241545"/>
+        /// </remarks>
+        public static bool IsTier1Equal<T>(this T self, T to, params string[] ignore) where T : class
+        {
+            if (self != null && to != null)
+            {
+                Type type = typeof(T);
+                List<string> ignoreList = new(ignore);
+                foreach (PropertyInfo pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (!ignoreList.Contains(pi.Name))
+                    {
+                        object selfValue = type.GetProperty(pi.Name).GetValue(self, null);
+                        object toValue = type.GetProperty(pi.Name).GetValue(to, null);
+
+                        if (selfValue != toValue && (selfValue == null || !selfValue.Equals(toValue)))
+                        {
+                            Console.WriteLine($"Mismatch at property {pi.Name}");
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            return self == to;
+        }
+
+
+
+        public static bool IsDeeplyEqual<T>(this T self, T to, params string[] ignore) where T : class
+        {
+            if (self != null && to != null)
+            {
+                Type type = typeof(T);
+                List<string> ignoreList = new(ignore);
+                foreach (PropertyInfo pi in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (!ignoreList.Contains(pi.Name))
+                    {
+                        object selfValue = type.GetProperty(pi.Name).GetValue(self, null);
+                        object toValue = type.GetProperty(pi.Name).GetValue(to, null);
+
+                        if (TypeExtensions.IsSimpleType(type.GetProperty(pi.Name).GetType()))
+                            if (selfValue != toValue && (selfValue == null || !selfValue.Equals(toValue)))
+                            {
+                                Console.WriteLine($"Mismatch at property {pi.Name}");
+                                return false;
+                            }
+                        else
+                            if (!IsDeeplyEqual(selfValue, toValue, ignore))
+                            {
+                                return false;
+                            }
+                                
+                    }
+                }
+                return true;
+            }
+            return self == to;
+        }
+
+    }
+
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Determine whether a type is simple (String, Decimal, DateTime, etc) 
+        /// or complex (i.e. custom class with public properties and methods).
+        /// </summary>
+        /// <see href="http://stackoverflow.com/questions/2442534/how-to-test-if-type-is-primitive"/>
+        public static bool IsSimpleType(this Type type)
+        {
+            return
+               type.IsValueType ||
+               type.IsPrimitive ||
+               new[]
+               {
+               typeof(String),
+               typeof(Decimal),
+               typeof(DateTime),
+               typeof(DateTimeOffset),
+               typeof(TimeSpan),
+               typeof(Guid)
+               }.Contains(type) ||
+               (Convert.GetTypeCode(type) != TypeCode.Object);
+        }
+
+        public static Type GetUnderlyingType(this MemberInfo member)
+        {
+            return member.MemberType switch
+            {
+                MemberTypes.Event => ((EventInfo)member).EventHandlerType,
+                MemberTypes.Field => ((FieldInfo)member).FieldType,
+                MemberTypes.Method => ((MethodInfo)member).ReturnType,
+                MemberTypes.Property => ((PropertyInfo)member).PropertyType,
+                _ => throw new ArgumentException
+                (
+                "Input MemberInfo must be if type EventInfo, FieldInfo, MethodInfo, or PropertyInfo"
+                ),
+            };
+        }
+    }
+}
---
+++ RollLogicTests/TestHelpersTests.cs
@@ -0,0 +1,165 @@
+﻿using NUnit.Framework;
+using RollLogicTests;
+using System;
+
+namespace RollLogicTests
+{
+    [TestFixture]
+    public class TestHelpers_Tests
+    {
+        private class SimpleType
+        {
+            public int I { get; set; }
+            public string Name { get; set; }
+            public DateTime DateTime { get; set; }
+        }
+
+
+        private class ComplexType : SimpleType
+        {
+            public SimpleType Simple1 { get; set; }
+        }
+
+
+
+        #region SIMPLE TYPES -----
+
+        [Test]
+        public void IsTier1Equal_SimpleTypesAllEqual_ReturnsTrue()
+        {
+            // Arrange
+            string TestString = "Fate is Fate";
+            SimpleType self = new()
+            {
+                I = 1, Name = TestString, DateTime = DateTime.Today
+            };
+            SimpleType to = new()
+            {
+                I = 1, Name = TestString, DateTime = DateTime.Today
+            };
+
+            // Act
+            var result = TestHelpers.IsTier1Equal(self, to);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+
+
+        [Test]
+        public void IsTier1Equal_SimpleTypes_UnequalStringRefButSameString_ReturnsTrue()
+        {
+            // Arrange
+            const string TestString = "Fate is Fate";
+            string a = new(TestString);
+            string b = TestString;
+            SimpleType self = new()
+            {
+                I = 1,
+                Name = a,
+                DateTime = DateTime.Today
+            };
+            SimpleType to = new()
+            {
+                I = 1,
+                Name = b,
+                DateTime = DateTime.Today
+            };
+            Assume.That(!ReferenceEquals(self.Name, to.Name));
+
+            // Act
+            var result = TestHelpers.IsTier1Equal(self, to);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        public void IsTier1Equal_SimpleTypesUnequal_ReturnsFalse()
+        {
+            // Arrange
+            string TestString = "Fate is Fate";
+            SimpleType self = new()
+            {
+                I = 1, Name = TestString, DateTime = DateTime.Today
+            };
+            SimpleType to = new()
+            {
+                I = 1, Name = TestString, DateTime = DateTime.Today.AddTicks(1)
+            };
+
+            // Act
+            var result = TestHelpers.IsTier1Equal(self, to);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+
+        [Test]
+        public void IsTier1Equal_ComplexContentsEqual_ReturnsFalse()
+        {
+            // Arrange
+            string TestString = "Fate is Fate";
+            ComplexType self = new()
+            {
+                I = 1,
+                Name = TestString,
+                DateTime = DateTime.Today
+            };
+            ComplexType to = new()
+            {
+                I = 1,
+                Name = TestString,
+                DateTime = DateTime.Today,
+                Simple1 = self
+            };
+            self.Simple1 = to;
+
+            // Act
+            var result = TestHelpers.IsTier1Equal(self, to);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        #endregion ---------------
+
+
+
+
+
+        #region COMPLEX TYPES ----
+
+        [Test]
+        public void IsDeeplyEqual_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string TestString = "Fate is Fate";
+            ComplexType self = new()
+            {
+                I = 1,
+                Name = TestString,
+                DateTime = DateTime.Today
+            };
+            ComplexType to = new()
+            {
+                I = 1,
+                Name = TestString,
+                DateTime = DateTime.Today,
+                Simple1 = self
+            };
+            self.Simple1 = to;
+
+            // Act
+            var result = TestHelpers.IsDeeplyEqual(self, to);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        #endregion ---------------
+    }
+}
