8b56c6c6ce5e91a3dcb2e69024cbfb0691abc6b2
SigurdJanson <emailjs@web.de>
2022-01-08 10:06:34 +0100

    New class to modify die rolls

---
+++ dev/RollLogic/RollChecks/ICheckModifier.cs
@@ -0,0 +1,14 @@
+﻿using FateExplorer.RollLogic;
+
+namespace FateExplorer.RollLogic
+{
+    public interface ICheckModifierM
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Before">An</param>
+        /// <returns></returns>
+        void Apply(IRollM Before);
+    }
+}
---
+++ dev/RollLogic/RollChecks/SimpleCheckModifierM.cs
@@ -0,0 +1,31 @@
+﻿namespace FateExplorer.RollLogic
+{
+    /// <summary>
+    /// The most common modifier. It simply adds the modifier to all rolled
+    /// dice. Used for basic ability, skill or combat checks.
+    /// </summary>
+    public class SimpleCheckModifierM : ICheckModifierM
+    {
+        /// <summary>
+        /// An additive modifier
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="value">The modifier's value to be applied to a roll</param>
+        public SimpleCheckModifierM(int value)
+        {
+            Value = value;
+        }
+
+
+        /// <inheritdoc/>
+        public void Apply(IRollM Before)
+        {
+            for (int i = 0; i < Before.OpenRoll.Length; i++)
+                Before.OpenRoll[i] -= Value;
+        }
+    }
+}
