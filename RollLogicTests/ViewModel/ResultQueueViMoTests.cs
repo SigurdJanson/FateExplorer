c8272c195c50f78e58fae7caa0f0025d13644e28
SigurdJanson <emailjs@web.de>
2022-01-08 18:39:42 +0100

    Queue to handle Roll Result, limit the list to a maximum and provide a reverse iterator.

---
+++ RollLogicTests/ViewModel/ResultQueueViMoTests.cs
@@ -0,0 +1,86 @@
+﻿using FateExplorer.RollLogic;
+using FateExplorer.ViewModel;
+using FateExplorer.FreeDiceCupViMo;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace RollLogicTests.ViewModel
+{
+    [TestFixture]
+    public class ResultQueueViMoTests
+    {
+        [Test]
+        public void Enqueue_ExceedMaxCount_LengthIsFixedtoMaxLen([Values(1, 2, 5, 10)] int MaxLen)
+        {
+            // Arrange
+            var resultQueueViMo = new ResultQueueViMo(MaxLen);
+            for (int i = 0; i < MaxLen; i++)
+            {
+                RollResultViMo item = new($"_{i + 1}", new int[1] { i }, CupType.Single);
+                resultQueueViMo.Enqueue(item);
+            }
+            Assume.That(resultQueueViMo.Count, Is.EqualTo(MaxLen));
+
+            // Act
+            resultQueueViMo.Enqueue(new($"_{99}", new int[1] { 99 }, CupType.Single));
+
+            // Assert
+            Assert.AreEqual(MaxLen, resultQueueViMo.Count);
+            if (MaxLen > 1)
+                Assert.AreEqual("_2", resultQueueViMo.Peek().Name);
+            else
+                Assert.AreEqual("_99", resultQueueViMo.Peek().Name);
+        }
+
+
+        [Test]
+        [TestCase(2, 1)]
+        [TestCase(5, 2)]
+        [TestCase(10, 5)]
+        public void Enqueue_DecreaseMaxCount_LengthIsReducedToFit(int Start, int Decreased)
+        {
+            // Arrange
+            var resultQueueViMo = new ResultQueueViMo(Start);
+            for (int i = 0; i < Start; i++)
+            {
+                RollResultViMo item = new($"_{i + 1}", new int[1] { i }, CupType.Single);
+                resultQueueViMo.Enqueue(item);
+            }
+            Assume.That(resultQueueViMo.Count, Is.EqualTo(Start));
+
+            // Act
+            resultQueueViMo.MaxCount = Decreased;
+
+            // Assert
+            Assert.AreEqual(Decreased, resultQueueViMo.Count);
+            Assert.AreEqual($"_{Start-Decreased+1}", resultQueueViMo.Peek().Name);
+        }
+
+
+
+        [Test]
+        public void Enqueue_Reverse_GetReversed([Values(2, 5, 10)] int MaxLen)
+        {
+            // Arrange
+            var resultQueueViMo = new ResultQueueViMo(MaxLen);
+            for (int i = 0; i < MaxLen; i++)
+            {
+                RollResultViMo item = new($"_{i + 1}", new int[1] { i }, CupType.Single);
+                resultQueueViMo.Enqueue(item);
+            }
+            Assume.That(resultQueueViMo.Count, Is.EqualTo(MaxLen));
+
+            // Act
+            // Assert
+            Assert.AreEqual(MaxLen, resultQueueViMo.Count);
+            int ri = MaxLen;
+            foreach (var item in resultQueueViMo.Reverse())
+            {
+                Assert.AreEqual($"_{ri}", item.Name);
+                ri--;
+            }
+                
+        }
+    }
+}
---
+++ dev/ViewModel/ResultQueueViMo.cs
@@ -0,0 +1,58 @@
+﻿using FateExplorer.RollLogic;
+using System.Collections.Generic;
+
+namespace FateExplorer.ViewModel
+{
+    public class ResultQueueViMo : Queue<RollResultViMo>
+    {
+        private int maxCount;
+
+        /// <summary>
+        /// The maximum of the queue. It will not hold more items than that.
+        /// if MaxCount <= 0 it will be ignored.
+        /// </summary>
+        public int MaxCount { 
+            get => maxCount; 
+            set
+            {
+                int oldMax = maxCount;
+                maxCount = value;
+                if (oldMax > value)
+                    ResetMax();
+            }
+        }
+
+        /// <summary>
+        /// Makes sure that the content of the queue does not exceed <see cref="MaxCount"/>
+        /// </summary>
+        protected void ResetMax()
+        {
+            if (MaxCount <= 0) return;
+            while (Count > MaxCount)
+                Dequeue();
+        }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="maxCount">THe maximum allowed elements in the queue</param>
+        public ResultQueueViMo(int maxCount = -1)
+        {
+            MaxCount = maxCount;
+        }
+
+
+
+
+        /// <summary>
+        /// Adds an object to the end of the queue.
+        /// </summary>
+        /// <param name="item">The object to add to the queue. The value can be null.</param>
+        public new void Enqueue(RollResultViMo item)
+        {
+            base.Enqueue(item);
+            ResetMax();
+        }
+    }
+}
