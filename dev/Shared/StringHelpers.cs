d655e8d7bec62ab7fd5686a6073c5464be25565d
SigurdJanson <emailjs@web.de>
2022-01-09 19:02:07 +0100

    Added a function that gives the number of commom leading characters between two files.

---
+++ dev/Shared/StringHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FateExplorer.Shared
+{
+    public static class StringHelpers
+    {
+        public static int Fitness(this string individual, string target)
+        {
+            int sum = 0;
+            for (int i = 0; i < Math.Min(individual.Length, target.Length); i++)
+                if (individual[i] == target[i]) sum++;
+            return sum;
+        }
+    }
+}
