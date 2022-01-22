4172c2c9bf5863513139bb06057f853c71169fab
SigurdJanson <emailjs@web.de>
2022-01-22 22:40:42 +0100

    New modifier that guarantess the given value

---
+++ RollLogicTests/RollLogic/RollChecks/ForcefulModifierTests.cs
@@ -0,0 +1,78 @@
+﻿using FateExplorer.RollLogic;
+using NUnit.Framework;
+using System;
+
+namespace RollLogicTests.RollLogic.RollChecks
+{
+    [TestFixture]
+    public class ForcefulModifierTests
+    {
+        [Test, Ignore("Not yet implemented")]
+        public void Apply_StateUnderTest_ExpectedBehavior(int New)
+        {
+            // Arrange
+            var forcefulModifier = new ForcefulModifier(New);
+            IRollM Before = null;
+
+            // Act
+            var result = forcefulModifier.Apply(
+                Before);
+
+            // Assert
+            Assert.Fail();
+        }
+
+
+
+        [Test]
+        public void Apply_Array_YieldsForcedValue(
+            [Random(0, 50, 10)] int Expected)
+        {
+            const int Length = 3;
+            // Arrange
+            var forcefulModifier = new ForcefulModifier(Expected);
+            int[] Before = new int[Length] { 2, 4, 6 };
+
+            // Act
+            var result = forcefulModifier.Apply(Before);
+
+            // Assert
+            Assert.AreEqual(
+                new int[Length] { Expected, Expected, Expected },
+                result);
+        }
+
+
+
+        [Test]
+        public void Apply_SingleValue_YieldsForcedValue(
+            [Random(0, 50, 5)] int Before,
+            [Random(0, 50, 5)] int Expected)
+        {
+            // Arrange
+            var forcefulModifier = new ForcefulModifier(Expected);
+
+            // Act
+            var result = forcefulModifier.Apply(Before);
+
+            // Assert
+            Assert.AreEqual(Expected, result);
+        }
+
+
+
+        //[Test, Ignore("Too simple")]
+        //public void Set_StateUnderTest_ExpectedBehavior()
+        //{
+        //    // Arrange
+        //    var forcefulModifier = new ForcefulModifier(2);
+        //    int value = 0;
+
+        //    // Act
+        //    forcefulModifier.Set(value);
+
+        //    // Assert
+        //    Assert.Fail();
+        //}
+    }
+}
---
+++ dev/RollLogic/RollChecks/ForcefulModifier.cs
@@ -0,0 +1,58 @@
+﻿namespace FateExplorer.RollLogic
+{
+    /// Modifier that forces a roll to a certain value. Used mostly when rolls yield no 
+    /// result (like no regeneration during the night, ...).
+    /// </summary>
+    public class ForcefulModifier : ICheckModifierM
+    {
+        /// <summary>
+        /// A value to force a roll/check result to
+        /// </summary>
+        public int Value { get; protected set; }
+
+
+        /// <inheritdoc/>
+        /// <remarks>Total cannot be interpreted for this type of modifier</remarks>
+        public int Total { get => 0; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ForcefulModifier(int value)
+        {
+            Value = value;
+        }
+
+
+        /// <inheritdoc/>
+        public int[] Apply(IRollM Before) // TODO: check for min/max???
+            => Apply(Before.OpenRoll);
+
+
+        /// <inheritdoc/>
+        public int[] Apply(int[] Before) // TODO: check for min/max???
+        {
+            int[] After = new int[Before.Length];
+
+            for (int i = 0; i < Before.Length; i++)
+                After[i] = Value;
+            return After;
+        }
+
+        /// <inheritdoc/>
+        public int Apply(int Before) // TODO: check for min/max???
+        {
+            return Value;
+        }
+
+
+
+        /// <inheritdoc/>
+        /// <remarks>Not implemented for this type of modifier</remarks>
+        public void Set(int value)
+        {
+            Value = value;
+        }
+
+    }
+}
