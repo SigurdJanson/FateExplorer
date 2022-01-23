9262bff587ad845f472e2756bf467b607fc871c4
SigurdJanson <emailjs@web.de>
2022-01-23 17:38:40 +0100

    New roll for the regeneration of energies

---
+++ RollLogicTests/RollLogic/EnergyRollMTests.cs
@@ -0,0 +1,24 @@
+﻿using FateExplorer.RollLogic;
+using NUnit.Framework;
+
+namespace RollLogicTests.RollLogic
+{
+    [TestFixture]
+    public class EnergyRollMTests
+    {
+        [Test]
+        public void Roll_NoModifier_ExpectedRange1to6()
+        {
+            // Arrange
+            var energyRollM = new EnergyRollM(RegenerationSite.Default, RegenerationDisturbance.None);
+
+            // Act
+            var result = energyRollM.Roll();
+
+            // Assert
+            Assert.AreEqual(1, result.Length);
+            Assert.GreaterOrEqual(result[0], 1);
+            Assert.LessOrEqual(result[0], 6);
+        }
+    }
+}
---
+++ dev/RollLogic/EnergyRollM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FateExplorer.RollLogic
+{
+    public class EnergyRollM : DieRollM
+    {
+        public const int _Sides = 6;
+
+        public ICheckModifierM SiteModifier { get; protected set; }
+        public ICheckModifierM DisturbModifier { get; protected set; }
+
+        public EnergyRollM(RegenerationSite site, RegenerationDisturbance disturb) : base(_Sides)
+        {
+            SiteModifier = site switch
+            {
+                RegenerationSite.Default => new SimpleCheckModifierM(0),
+                RegenerationSite.Good => new SimpleCheckModifierM(1),
+                RegenerationSite.Poor => new SimpleCheckModifierM(-1),
+                RegenerationSite.Bad => new HalfModifier(),
+                RegenerationSite.Terrible => new ForcefulModifier(0),
+                _ => throw new NotImplementedException()
+            };
+            DisturbModifier = disturb switch
+            {
+                RegenerationDisturbance.None => new SimpleCheckModifierM(0),
+                RegenerationDisturbance.Brief => new SimpleCheckModifierM(-1),
+                RegenerationDisturbance.Prolonged => new SimpleCheckModifierM(-2),
+                _ => throw new NotImplementedException()
+            };
+
+        }
+
+        /// <inheritdoc/>
+        public override int[] Roll()
+        {
+            base.Roll();
+
+            int BeforeMod = OpenRoll[0];
+            OpenRoll[0] = DisturbModifier.Apply(OpenRoll[0]);
+            OpenRoll[0] = SiteModifier.Apply(OpenRoll[0]);
+            // energy regeneration cannot be below 0
+            if (OpenRoll[0] < 0) OpenRoll[0] = 0;
+
+            ModifiedBy = new int[1] { OpenRoll[0] - BeforeMod };
+
+            return OpenRoll;
+        }
+
+
+    }
+}
