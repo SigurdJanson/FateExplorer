aafcbffacefc27eee6076468f4f04a11e31ddee2
SigurdJanson <emailjs@web.de>
2022-01-29 12:19:43 +0100

    Make RNG mock available to all tests

---
+++ RollLogicTests/RollLogic/MockedRng.cs
@@ -0,0 +1,29 @@
+﻿using FateExplorer.RollLogic;
+
+namespace RollLogicTests.RollLogic
+{
+    public class MockedRng : IRandomNG
+    {
+        public int ForcedIValue { get; set; }
+
+        public uint BRandom()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int IRandom(int min, int max)
+        {
+            return ForcedIValue;
+        }
+
+        public double Random()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RandomInit(uint seed)
+        {
+            ForcedIValue = (int)seed;
+        }
+    }
+}
--- RollLogicTests/RollLogic/MultiDieRollTests.cs
+++ RollLogicTests/RollLogic/MultiDieRollTests.cs
@@ -5,30 +5,6 @@ using System.Linq;
 
 namespace RollLogicTests.RollLogic
 {
-    public class MockedRng : IRandomNG
-    {
-        public int ForcedIValue { get; set; }
-
-        public uint BRandom()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int IRandom(int min, int max)
-        {
-            return ForcedIValue;
-        }
-
-        public double Random()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void RandomInit(uint seed)
-        {
-            ForcedIValue = (int)seed;
-        }
-    }
 
 
 
