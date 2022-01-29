c848175828c59a65b6af57ecfbbde693e7a59c90
SigurdJanson <emailjs@web.de>
2022-01-29 12:42:06 +0100

    Tests and improvements on ArrayByEnum: iterator is now typed.

---
+++ RollLogicTests/Shared/ArrayByEnumTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace RollLogicTests.Shared
+{
+    [TestFixture]
+    public class ArrayByEnumTests
+    {
+        enum TestEnum { Zero = 0, One = 1, Two = 2, Three = 3, Four = 4, Five = 5 }
+
+
+
+        [Test]
+        public void IndexOperator()
+        {
+            // Arrange
+            var arrayByEnum = new ArrayByEnum<string, TestEnum>();
+            var enumValues = Enum.GetValues(typeof(TestEnum)).Cast<TestEnum>();
+
+            // Act
+            foreach (TestEnum e in enumValues)
+            {
+                arrayByEnum[e] = $"{(int)e}";
+            }
+
+            // Assert
+            Assert.AreEqual(new string[] { "0", "1", "2", "3", "4", "5" }, arrayByEnum);
+        }
+
+
+
+        [Test]
+        public void Iterator()
+        {
+            // Arrange
+            var arrayByEnum = new ArrayByEnum<string, TestEnum>();
+            arrayByEnum[TestEnum.Zero] = "0000000000";
+            arrayByEnum[TestEnum.One] = "1";
+            arrayByEnum[TestEnum.Two] = "02";
+            arrayByEnum[TestEnum.Three] = "003";
+            arrayByEnum[TestEnum.Four] = "FooooUUUURRR";
+            arrayByEnum[TestEnum.Five] = "00005";
+
+            // Act
+            foreach (var e in arrayByEnum)
+            {
+                Assume.That(e is not null);
+            }
+
+            // Assert
+            
+        }
+    }
+}
--- dev/Shared/ArrayByEnum.cs
+++ dev/Shared/ArrayByEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RollLogicTests.Shared
@@ -8,7 +9,7 @@ namespace RollLogicTests.Shared
     /// <typeparam name="T">Type stored in array</typeparam>
     /// <typeparam name="U">Indexer Enum type</typeparam>
     /// <remarks>With regards to <seealso href="https://stackoverflow.com/a/50969107/13241545"/></remarks>
-    public class ArrayByEnum<T, U> : IEnumerable where U : Enum
+    public class ArrayByEnum<T, U> : IEnumerable<T> where U : Enum
     {
         private readonly T[] _array;
         private readonly int _lower;
@@ -37,10 +38,17 @@ namespace RollLogicTests.Shared
         /// <summary>
         /// Gets the number of elements contained in the array
         /// </summary>
-        public int Count => typeof(U).GetFields().Length; //Enum.GetNames(typeof(U)).Length;
+        public int Count => typeof(U).GetFields().Length;
 
         /// <inheritdoc />
-        public IEnumerator GetEnumerator()
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Enum.GetValues(typeof(U)).Cast<U>().Select(i => this[i]).GetEnumerator();
+        }
+
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
         {
             return Enum.GetValues(typeof(U)).Cast<U>().Select(i => this[i]).GetEnumerator();
         }
