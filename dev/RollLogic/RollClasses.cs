e418dd060b761564764d9e74682642c7a5019d2b
SigurdJanson <emailjs@web.de>
2022-01-30 21:37:12 +0100

    Refactor: removed unnecessary code

--- dev/RollLogic/RollClasses.cs
+++ dev/RollLogic/RollClasses.cs
@@ -1,114 +1,34 @@
-﻿using FateExplorer.Shared;
-using System;
-
+﻿
 namespace FateExplorer.RollLogic
 {
     public class D20Roll : DieRollM
     {
-        public const int _Sides = 20;
-        public RollSuccessLevel Meaning { get; protected set; }
+        protected const int _Sides = 20;
 
         public D20Roll() : base(_Sides)
         {
         }
 
-
-        public RollSuccessLevel Verify(int Threshold)
-        {
-            if (Threshold < 1 || Threshold > Sides[0])
-                throw new ArgumentOutOfRangeException(nameof(Threshold));
-
-            RollSuccessLevel Success;
-            if (OpenRoll[0] == 20)
-                Success = RollSuccessLevel.PendingBotch;
-            else if (OpenRoll[0] == 1)
-                Success = RollSuccessLevel.PendingCritical;
-            else
-                Success = OpenRoll[0] <= Threshold ? RollSuccessLevel.Success : RollSuccessLevel.Fail;
-
-            Meaning = Success;
-            return Success;
-        }
-    }
-
-    // Probably obsolete
-    public class D20ConfirmRoll : DieRollM
-    {
-        public const int _Sides = 20;
-
-        public RollSuccessLevel Meaning { get; protected set; }
-
-        public D20ConfirmRoll() : base(_Sides)
-        {
-        }
-
-
-        public RollSuccessLevel Verify(D20Roll Roll, int Threshold)
-        {
-            if (Threshold < 1 || Threshold > Sides[0])
-                throw new ArgumentOutOfRangeException(nameof(Threshold));
-
-            RollSuccessLevel Success;
-            if (Roll.Meaning == RollSuccessLevel.PendingBotch)
-            {
-                if (OpenRoll[0] <= Threshold)
-                    Success = RollSuccessLevel.Fail;
-                else
-                    Success = RollSuccessLevel.Botch;
-            }
-            else if (Roll.Meaning == RollSuccessLevel.PendingCritical)
-            {
-                if (OpenRoll[0] <= Threshold)
-                    Success = RollSuccessLevel.Critical;
-                else
-                    Success = RollSuccessLevel.Success;
-            }
-            else
-            {
-                Success = Roll.Meaning;
-            }
-
-            Meaning = Success;
-            return Success;
-        }
     }
 
 
     public class SkillRoll : MultiDieRoll
     {
-        public const int _Sides = 20;
-        public const int _DieCount = 3;
-        public RollSuccessLevel Meaning { get; protected set; }
+        protected const int _Sides = 20;
+        protected const int _DieCount = 3;
 
         public SkillRoll() : base(_Sides, _DieCount)
         {
         }
 
-
-        public RollSuccessLevel Verify(int Threshold)
-        {
-            if (Threshold < 1 || Threshold > Sides[0])
-                throw new ArgumentOutOfRangeException(nameof(Threshold));
-
-            RollSuccessLevel Success;
-            if (OpenRoll[0] == 20)
-                Success = RollSuccessLevel.PendingBotch;
-            else if (OpenRoll[0] == 1)
-                Success = RollSuccessLevel.PendingCritical;
-            else
-                Success = OpenRoll[0] <= Threshold ? RollSuccessLevel.Success : RollSuccessLevel.Fail;
-
-            Meaning = Success;
-            return Success;
-        }
     }
 
 
 
     public class BotchEffectRoll : MultiDieRoll
     {
-        public const int _Sides = 6;
-        public const int _DieCount = 2;
+        protected const int _Sides = 6;
+        protected const int _DieCount = 2;
 
         public BotchEffectRoll() : base(_Sides, _DieCount)
         {
