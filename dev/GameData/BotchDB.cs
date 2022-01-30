2672c8bed6f0953e297c4ab5c7dfb59a697c2a75
SigurdJanson <emailjs@web.de>
2022-01-30 10:09:08 +0100

    Enhanced documentation

--- dev/GameData/BotchDB.cs
+++ dev/GameData/BotchDB.cs
@@ -4,9 +4,9 @@ using System.Text.Json.Serialization;
 
 namespace FateExplorer.GameData
 {
-
-    // BotchProvider myDeserializedClass = JsonConvert.DeserializeObject<BotchProvider>(myJsonResponse); 
-
+    /// <summary>
+    /// Access to the botch effects as specified in the rule book.
+    /// </summary>
     public class BotchDB
     {
         [JsonPropertyName("Effects")]
@@ -16,6 +16,15 @@ namespace FateExplorer.GameData
         public IReadOnlyList<BotchTable> Tables { get; set; }
 
 
+        /// <summary>
+        /// Returns the overview of all botch effects for a certain roll
+        /// </summary>
+        /// <param name="Roll">A unique identifier for the particular check, parry, dodge, skill</param>
+        /// <param name="Type">
+        /// A unique identifier to specifiy the type of combat check (shield, umarmed, ranged) or skill (arcane or karma).
+        /// </param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException"></exception>
         public BotchTable GetBotchTable(string Roll, string Type)
         {
             foreach (var t in Tables)
@@ -30,6 +39,13 @@ namespace FateExplorer.GameData
         }
 
 
+        /// <summary>
+        /// Returns a particular botch effect.
+        /// </summary>
+        /// <inheritdoc cref="GetBotchTable(string, string)"/>
+        /// <param name="DiceEyes">The result of the botch effect roll (2d6).</param>
+        /// <returns></returns>
+        /// <exception cref="KeyNotFoundException"></exception>
         public BotchEntry GetBotch(string Roll, string Type, int DiceEyes)
         {
             // Find correct table in `Tables`
@@ -49,10 +65,15 @@ namespace FateExplorer.GameData
                         return b;
 
             // if nothing found: error
-            throw new Exception("Unknown botch effect");
+            throw new KeyNotFoundException("Unknown botch effect");
         }
 
-
+        /// <summary>
+        /// Return a relative URL that points to a documentation of the given botch
+        /// </summary>
+        /// <param name="Roll"></param>
+        /// <param name="Type"></param>
+        /// <returns></returns>
         public string GetUrl(string Roll, string Type)
         {
             // Find correct table in `Tables`
