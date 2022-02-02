f8b497073678ea099855279034f8f74da63e2316
SigurdJanson <emailjs@web.de>
2022-02-02 22:17:32 +0100

    Documentation

--- dev/GameData/DataServiceCollectionBase.cs
+++ dev/GameData/DataServiceCollectionBase.cs
@@ -6,13 +6,29 @@ namespace FateExplorer.GameData
 {
     public class DataServiceCollectionBase<T> where T : ICharacterAttribute
     {
+        /// <summary>
+        /// List containing the data base entries
+        /// </summary>
         [JsonPropertyName("Entries")]
         public IReadOnlyList<T> Data { get; set; }
 
+        /// <summary>
+        /// Return the element of the data base with the designated numeric index.
+        /// </summary>
+        /// <param name="i">i-th item</param>
+        /// <returns>Data base object at index i</returns>
         public T this[int i] => Data[i];
 
+        /// <summary>
+        /// Return the element of the data base with the designated id string.
+        /// </summary>
+        /// <param name="IdString">A unique string id</param>
+        /// <returns>The object with the given id; default if the id does not exist.</returns>
         public T this[string IdString] => Data.First(i => i.Id == IdString);
 
+        /// <summary>
+        /// Number of entries in the data base
+        /// </summary>
         [JsonIgnore]
         public int Count { get => Data?.Count ?? 0; }
     }
