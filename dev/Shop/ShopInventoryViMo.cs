a5e583cd236af6badb9751a27a8bf2ff1ed7eb54
SigurdJanson <emailjs@web.de>
2022-02-07 22:13:02 +0100

    Shop now filters by text and category

--- dev/Shop/ShopInventoryViMo.cs
+++ dev/Shop/ShopInventoryViMo.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -8,23 +10,32 @@ namespace FateExplorer.Shop
     public class ShopInventoryViMo
     {
         protected HttpClient DataSource;
+        protected IStringLocalizer<App> l10n;
 
 
-        public ShopInventoryViMo(HttpClient dataSource)
+        public ShopInventoryViMo(HttpClient dataSource, IStringLocalizer<App> localizer)
         {
             DataSource = dataSource;
+            l10n = localizer;
         }
 
         protected List<ShopItemM> Inventory { get; set; }
 
 
-        public List<ShopItemViMo> GetStock(string Filter)
+        public List<ShopItemViMo> GetStock(string Filter, int? GroupId)
         {
             List<ShopItemM> Selected;
-            if (Filter is not null)
-                Selected = Inventory.FindAll(i => i.Name.Contains(Filter, System.StringComparison.CurrentCultureIgnoreCase));
-            else
-                Selected = new();
+
+            Selected = (string.IsNullOrWhiteSpace(Filter), GroupId is null) switch
+            {
+                (true, true) => Inventory,
+                (true, false) => Inventory.FindAll(i => (int)i.Group == GroupId),
+                (false, true) => Inventory.FindAll(i => i.Name.Contains(Filter, StringComparison.CurrentCultureIgnoreCase) ||
+                       l10n[i.Group.ToString()].Value.Contains(Filter, StringComparison.CurrentCultureIgnoreCase)),
+                (false, false) => Inventory.FindAll(i => (int)i.Group == GroupId)
+                                           .FindAll(i => i.Name.Contains(Filter, StringComparison.CurrentCultureIgnoreCase))
+            };
+            if (Selected is null) Selected = new();
 
             List<ShopItemViMo> Result = new();
             foreach (var item in Selected)
@@ -32,6 +43,29 @@ namespace FateExplorer.Shop
 
             return Result;
         }
+
+
+        /// <summary>
+        /// Get the localised names of the group names (represented by <see cref="ShopItemM.GroupId"/>
+        /// </summary>
+        /// <returns>The names as tupels with the id as int and the name</returns>
+        public IEnumerable<(int id, string name)> GetGroups()
+        {
+            //string[] Groups = Enum.GetNames(typeof(ShopItemM.GroupId));
+            //string[] GroupNames = new string[Groups.Length];
+            //for (int g = 0; g < Groups.Length; g++)
+            //{
+            //    GroupNames[g] = l10n[Groups[g]];
+            //}
+            //return GroupNames;
+            List<(int id, string name)> Groups = new();
+            foreach (var g in Enum.GetValues(typeof(ShopItemM.GroupId)))
+            {
+                Groups.Add(((int)g, l10n[g.ToString()]));
+            }
+            return Groups;
+        }
+        
 
         /// <summary>
         /// Load the data
