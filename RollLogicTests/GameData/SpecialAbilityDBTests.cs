5b2ac53d10f88c1ac73f638a1ea454c60054f52a
SigurdJanson <emailjs@web.de>
2022-02-20 22:47:25 +0100

    Special abilities in game data base

---
+++ RollLogicTests/GameData/SpecialAbilityDBTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FateExplorer.GameData;
+using NUnit.Framework;
+
+
+namespace RollLogicTests.GameData
+{
+    [TestFixture]
+    public class SpecialAbilityDBTests : GameDataTestsBase<SpecialAbilityDB, SpecialAbilityDbEntry>
+    {
+        public override string FilenameId { get => "specabs"; }
+
+
+        [Test]
+        [TestCase("de", "Abgeschwächte Liturgie", "Weg d. Zofe")]
+        //[TestCase("en", "Basilisk-tongue", "Guild Magic Ball"), Ignore("")]
+        public void LoadFromFile_ParseSuccessful(string Language, string SA1, string SALast)
+        {
+            // Arrange
+            // Act
+            SpecialAbilityDB Result = CreateDBfromFile(Language);
+
+            // Assert
+            Assert.AreEqual(1088, Result.Count);
+            Assert.AreEqual(SA1, Result[0].Name);
+            Assert.AreEqual(SALast, Result[^1].Name);
+        }
+
+        [Test, Ignore("No english version, yet")]
+        [TestCase("", new string[] { "de", "en" })]
+        public override void CompareLanguages_Equality(string Dummy, string[] Languages)
+        {
+
+        }
+
+        /// inherited: public void Count_ContentNotLoaded_Return0()
+    }
+}
---
+++ dev/GameData/SpecialAbilityDB.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FateExplorer.GameData
+{
+
+    public enum SpecAbGroups { 
+        General =  1,
+        ExtendedSkills =  2,
+        Skillstiles =  3,
+        Commands =  4,
+        Combat =  5,
+        CombatStiles =  6,
+        CombatExtended =  7,
+        CombatBrawl =  8,
+        FatePoint =  9,
+        BlessedGeneral = 51,
+        BlessedExtended = 52,
+        BlessedTraditions = 53,
+        LiturgyStiles = 54,
+        Sermons = 55,
+        Visions = 56,
+        CeremonialObjects = 57,
+        MagicGeneral = 21,
+        MagicExtended = 22,
+        MagicTraditions = 23,
+        MagicStiles = 24,
+        MagicSigns = 25,
+        Traditionsartefakte = 26,
+        LycanthropicGifts = 27,
+        Homunculus = 28,
+        SikaryanPredation = 29,
+        PactGifts = 30}
+
+
+    public class SpecialAbilityDB : DataServiceCollectionBase<SpecialAbilityDbEntry>
+    {
+        // inherited
+
+        /// <summary>
+        /// List containing the data base entries
+        /// </summary>
+        [JsonPropertyName("groups")]
+        public IReadOnlyList<SpecialAbilityDbGroup> GroupNames { get; set; }
+
+    }
+
+
+    public class SpecialAbilityDbGroup
+    {
+        [JsonPropertyName("id")]
+        public SpecAbGroups Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+
+
+    public class SpecialAbilityDbEntry : ICharacterAttribute
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("group")]
+        public SpecAbGroups Group { get; set; }
+
+
+        // TODO: no URLs in file
+    }
+}
