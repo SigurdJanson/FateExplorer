270d0a464cdf21f1442ff83e4b2ed91ef5616a82
SigurdJanson <emailjs@web.de>
2022-03-13 19:47:22 +0100

    Added new Add method that considers duplicates; ItemCount counts all items in cart, even duplicates.

--- dev/Shop/ShoppingCartViMo.cs
+++ dev/Shop/ShoppingCartViMo.cs
@@ -20,6 +20,19 @@ namespace FateExplorer.Shop
             }
         }
 
+        public int ItemCount
+        {
+            get
+            {
+                int Result = 0;
+                ForEach(delegate ((ShopItemViMo item, int count) i)
+                {
+                    Result += i.count;
+                });
+                return Result;
+
+            }
+        }
 
         /// <summary>
         /// Removes all items with a count of 0.
@@ -29,6 +42,22 @@ namespace FateExplorer.Shop
             for (int i = 0; i < Count; i++)
                 if (this[i].Count == 0)
                     RemoveAt(i);
+        }
+
+
+        public new void Add((ShopItemViMo Item, int Count) item)
+        {
+            if (item.Count == 0) return;
+
+            for (int i = 0; i < Count; i++)
+                if (this[i].Item == item.Item)
+                {
+                    int ExistingCount = this[i].Count;
+                    item.Count += ExistingCount;
+                    RemoveAt(i);
+                }
+
+            base.Add(item);
         }
     }
 }
