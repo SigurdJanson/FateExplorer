ab856443dab3d99528af537b9539e71e30775853
SigurdJanson <emailjs@web.de>
2022-03-20 13:15:29 +0100

    Removed tests for methods that are now private

--- RollLogicTests/RollLogic/RollSuccessTests.cs
+++ RollLogicTests/RollLogic/RollSuccessTests.cs
@@ -108,49 +108,8 @@ namespace vmCode_UnitTests.RollLogic
 
 
 
-        [Test]
-        [TestCase(1, 1, ExpectedResult = RollSuccess.Level.PendingCritical)]
-        [TestCase(2, 1, ExpectedResult = RollSuccess.Level.Fail)]
-        [TestCase(1, 2, ExpectedResult = RollSuccess.Level.PendingCritical)]
-        [TestCase(2, 2, ExpectedResult = RollSuccess.Level.Success)]
-        [TestCase(20, 20, ExpectedResult = RollSuccess.Level.PendingBotch)]
-        [TestCase(19, 20, ExpectedResult = RollSuccess.Level.Success)]
-        [TestCase(20, 19, ExpectedResult = RollSuccess.Level.PendingBotch)]
-        [TestCase(19, 19, ExpectedResult = RollSuccess.Level.Success)]
-        public RollSuccess.Level PrimaryD20Success_DefaultBotchThreshold_Permutations(int Eyes, int Attr)
-        {
-            // Arrange
-            var rollSuccess = new RollSuccess();
-
-            // Act
-            var result = rollSuccess.PrimaryD20Success(Eyes, Attr);
-
-            // Assert
-            return result;
-        }
 
 
-
-        [Test]
-        [TestCase(1, 1, ExpectedResult = RollSuccess.Level.Critical)]
-        [TestCase(2, 1, ExpectedResult = RollSuccess.Level.Fail)]
-        [TestCase(1, 2, ExpectedResult = RollSuccess.Level.Critical)]
-        [TestCase(2, 2, ExpectedResult = RollSuccess.Level.Success)]
-        [TestCase(20, 20, ExpectedResult = RollSuccess.Level.Botch)]
-        [TestCase(19, 20, ExpectedResult = RollSuccess.Level.Success)]
-        [TestCase(20, 19, ExpectedResult = RollSuccess.Level.Botch)]
-        [TestCase(19, 19, ExpectedResult = RollSuccess.Level.Success)]
-        public RollSuccess.Level D20Success_Permutations(int Eyes, int Attr)
-        {
-            // Arrange
-            var rollSuccess = new RollSuccess();
-
-            // Act
-            var result = rollSuccess.D20Success(Eyes, Attr);
-
-            // Assert
-            return result;
-        }
 
         [Test, TestCaseSource(nameof(LevelCombinations))]
         public void staticCheckSuccess_Combinations(RollSuccess.Level Prim, RollSuccess.Level Conf, RollSuccess.Level expected)
