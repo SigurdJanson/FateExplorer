1c5fd4efb1d58f8331641d11dfd757fbcef05657
SigurdJanson <emailjs@web.de>
2022-04-03 13:32:41 +0200

    Added class to hold names of fate explorer icons and use it

---
+++ dev/Shared/IconsFE.cs
@@ -0,0 +1,24 @@
+﻿//using MudBlazor;
+
+namespace MudBlazor
+{
+    public static class IconsFE
+    {
+        const string Ligaturisation = "fe ";
+        const string Prefix = "fe-";
+        public static string Success => Ligaturisation + Prefix + nameof(Success);
+        public static string Botch => Ligaturisation + Prefix + nameof(Botch);
+        public static string PotionArcane => Ligaturisation + Prefix + nameof(PotionArcane);
+        public static string PotionLife => Ligaturisation + Prefix + nameof(PotionLife);
+        public static string D6_1 => Ligaturisation + Prefix + nameof(D6_1);
+        public static string D6_2 => Ligaturisation + Prefix + nameof(D6_2);
+        public static string D6_3 => Ligaturisation + Prefix + nameof(D6_3);
+        public static string D6_4 => Ligaturisation + Prefix + nameof(D6_4);
+        public static string D6_5 => Ligaturisation + Prefix + nameof(D6_5);
+        public static string D6_6 => Ligaturisation + Prefix + nameof(D6_6);
+        public static string LocalMallClear => Ligaturisation + Prefix + nameof(LocalMallClear);
+        public static string Ulisses => Ligaturisation + Prefix + nameof(Ulisses);
+        public static string BackHandLeft => Ligaturisation + Prefix + nameof(BackHandLeft);
+        public static string Sword => Ligaturisation + Prefix + nameof(Sword);
+    }
+}
