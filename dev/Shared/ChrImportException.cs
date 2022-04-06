6c0f712820da0851fdce54995748a8e71e30aafb
SigurdJanson <emailjs@web.de>
2022-04-06 23:27:41 +0200

    Better import messages for errors

---
+++ dev/Shared/ChrImportException.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Security.Permissions;
+
+namespace FateExplorer.Shared
+{
+    [Serializable]
+    public class ChrImportException : Exception
+    {
+        /// <summary>
+        /// The piece of information that is the cause for a failed import
+        /// </summary>
+        public enum Property 
+        { 
+            /// <summary>
+            /// The JSON format does not fit expectations
+            /// </summary>
+            Format, 
+            /// <summary>
+            /// The basic character specification cannot be interpreted (e.g. species, gender, or name)
+            /// </summary>
+            Specification, 
+            /// <summary>
+            /// An attribute (ability, derived characteristic, ...)
+            /// </summary>
+            Attribute, 
+            /// <summary>
+            /// A skill, spell, cantrip, liturgy, ...
+            /// </summary>
+            Skills, 
+            /// <summary>
+            /// A purchasable item, a weapon or something else
+            /// </summary>
+            Belonging, 
+            /// <summary>
+            /// A pet or other specified companion
+            /// </summary>
+            Companion,
+            /// <summary>
+            /// Relationships like pacts, ...
+            /// </summary>
+            Ties,
+            /// <summary>
+            /// The cause a failed import is unclear
+            /// </summary>
+            Undefined
+        }
+
+        /// <summary>
+        /// The cause of the failed import
+        /// </summary>
+        protected Property Cause { get; set; }
+
+        public string GetCause() => Cause.ToString();
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChrImportException(Property cause = Property.Undefined) : base() 
+        {
+            Cause = cause;
+        }
+
+        public ChrImportException(string message, Property cause = Property.Undefined)
+            : base(message)
+        {
+            Cause = cause;
+        }
+
+        public ChrImportException(string message, Exception inner, Property cause = Property.Undefined)
+            : base(message, inner)
+        {
+            Cause = cause;
+        }
+
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client.
+        protected ChrImportException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+
+        // GetObjectData performs a custom serialization. 
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        {
+            // ...
+            info.AddValue("Cause", Cause);
+
+            base.GetObjectData(info, context);
+        }
+    }
+}
