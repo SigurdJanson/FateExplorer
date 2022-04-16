9f1281ebe520c680ad0f480b543654c544a93093
SigurdJanson <emailjs@web.de>
2022-04-16 22:03:52 +0200

    Cleaned up some unneeded code

--- dev/Shop/CurrencyM.cs
+++ dev/Shop/CurrencyM.cs
@@ -1,5 +1,4 @@
 ﻿using FateExplorer.GameData;
-using System.Text.Json.Serialization;
 
 namespace FateExplorer.Shop
 {
@@ -14,30 +13,12 @@ namespace FateExplorer.Shop
         }
 
 
-        [JsonConstructor]
-        public CurrencyM(
-            string id,
-            string origin,
-            string name,
-             double rate
-        )
-        {
-            this.Id = id;
-            this.Origin = origin;
-            this.Name = name;
-            this.Rate = rate;
-        }
-
-        [JsonPropertyName("id")]
         public string Id { get; }
 
-        [JsonPropertyName("origin")]
         public string Origin { get; }
 
-        [JsonPropertyName("name")]
         public string Name { get; }
 
-        [JsonPropertyName("rate")]
         public double Rate { get; }
     }
 }
