ecf243e48dc16c12a91136d168d1f92c15467f6b
SigurdJanson <emailjs@web.de>
2022-04-23 15:19:33 +0200

    Added class BosparanCalendar

---
+++ RollLogicTests/Calendar/BosparanCalendarTests.cs
@@ -0,0 +1,520 @@
+﻿using FateExplorer.Calendar;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests.Calendar;
+
+
+#pragma warning disable IDE0018 // Inlinevariablendeklaration
+
+
+[TestFixture]
+public class BosparanCalendarTests
+{
+
+    #region Get Functions
+
+    [Test]
+    [TestCase("18.4.2022", ExpectedResult = 4)]
+    [TestCase("26.12.2005", ExpectedResult = 12)]
+    [TestCase("27.12.2005", ExpectedResult = 13)]
+    [TestCase("29.06.1987", ExpectedResult = 6)]
+    [TestCase("30.06.1987", ExpectedResult = 7)]
+    [TestCase("29.06.1988", ExpectedResult = 6)] // Leap year
+    [TestCase("30.06.1988", ExpectedResult = 7)] // Leap year
+    public int GetMonth(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetMonth(time);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase("18.4.2022", ExpectedResult = 1045)]
+    [TestCase("26.12.2005", ExpectedResult = 1028)]
+    [TestCase("27.12.2005", ExpectedResult = 1028)]
+    [TestCase("30.06.1987", ExpectedResult = 1010)]
+    public int GetYear(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetYear(time);
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase("30.1.2000", ExpectedResult = 11)] // Leap year
+    [TestCase("31.1.2000", ExpectedResult = 11)] // Leap year
+    [TestCase("18.4.2022", ExpectedResult = 11)]
+    [TestCase("26.12.2005", ExpectedResult = 11)]
+    [TestCase("27.12.2005", ExpectedResult = 11)]
+    [TestCase("30.06.1988", ExpectedResult = 11)] // Leap year
+    public int GetEra_Always11(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetEra(time);
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase("30.1.2000", ExpectedResult = 30)]
+    [TestCase("31.1.2000", ExpectedResult = 1)]
+    [TestCase("18.4.2022", ExpectedResult = 18)]
+    [TestCase("26.12.2005", ExpectedResult = 30)]
+    [TestCase("27.12.2005", ExpectedResult = 1)]
+    [TestCase("29.06.1987", ExpectedResult = 30)]
+    [TestCase("30.06.1987", ExpectedResult = 1)]
+    [TestCase("29.06.1988", ExpectedResult = 30)] // Leap year
+    [TestCase("30.06.1988", ExpectedResult = 1)] // Leap year
+    public int GetDayOfMonth(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetDayOfMonth(time);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase("13.4.2022", ExpectedResult = DayOfWeek.Saturday)] // Reference -1
+    [TestCase("14.4.2022", ExpectedResult = DayOfWeek.Sunday)] // Reference date
+    [TestCase("15.4.2022", ExpectedResult = DayOfWeek.Monday)] // Reference +1
+    [TestCase("30.1.2000", ExpectedResult = DayOfWeek.Tuesday)]
+    [TestCase("31.1.2000", ExpectedResult = DayOfWeek.Wednesday)]
+    [TestCase("26.12.2005", ExpectedResult = DayOfWeek.Monday)]
+    [TestCase("27.12.2005", ExpectedResult = DayOfWeek.Tuesday)]
+    [TestCase("30.06.1987", ExpectedResult = DayOfWeek.Sunday)]
+    [TestCase("18.4.2022", ExpectedResult = DayOfWeek.Thursday)]
+    [TestCase("28.9.2025", ExpectedResult = DayOfWeek.Tuesday)]
+    public DayOfWeek GetDayOfWeek(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetDayOfWeek(time);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase("30.1.2000", ExpectedResult = 30)]
+    [TestCase("31.1.2000", ExpectedResult = 31)]
+    [TestCase("18.4.2022", ExpectedResult = 3 * 30 + 18)]
+    [TestCase("26.12.2005", ExpectedResult = 12 * 30)]
+    [TestCase("27.12.2005", ExpectedResult = 12 * 30 + 1)]
+    [TestCase("30.06.1987", ExpectedResult = 31 + 28 + 31 + 30 + 31 + 30)]
+    public int GetDayOfYear(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetDayOfYear(time);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase("30.1.2000", ExpectedResult = 365)] // Leap year
+    [TestCase("31.1.2000", ExpectedResult = 365)] // Leap year
+    [TestCase("18.4.2022", ExpectedResult = 365)]
+    [TestCase("26.12.2005", ExpectedResult = 365)]
+    [TestCase("27.12.2005", ExpectedResult = 365)]
+    [TestCase("30.06.1988", ExpectedResult = 365)] // Leap year
+    public int GetDaysInYear(string Date)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetDaysInYear(time.Year);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase("30.1.2000", 11, ExpectedResult = 365)] // Leap year
+    [TestCase("31.1.2000", 12, ExpectedResult = 365)] // Leap year
+    [TestCase("18.4.2022", 11, ExpectedResult = 365)]
+    [TestCase("26.12.2005", 10, ExpectedResult = 365)]
+    [TestCase("27.12.2005", 11, ExpectedResult = 365)]
+    [TestCase("30.06.1988", 11, ExpectedResult = 365)] // Leap year
+    public int GetDaysInYear_Era_NoDifference(string Date, int Era)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetDaysInYear(time.Year, Era);
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase("30.1.2000", 11, ExpectedResult = 13)] // Leap year
+    [TestCase("31.1.2000", 12, ExpectedResult = 13)] // Leap year
+    [TestCase("18.4.2022", 11, ExpectedResult = 13)]
+    [TestCase("26.12.2005", 10, ExpectedResult = 13)]
+    [TestCase("27.12.2005", 11, ExpectedResult = 13)]
+    [TestCase("30.06.1988", 11, ExpectedResult = 13)] // Leap year
+    public int GetMonthsInYear_Era_NoDifference(string Date, int Era)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.GetMonthsInYear(time.Year, Era);
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase(2000, 1, 11, ExpectedResult = 30)]
+    [TestCase(2022, 4, 11, ExpectedResult = 30)]
+    [TestCase(2005, 12, 10, ExpectedResult = 30)]
+    [TestCase(2005, 13, 11, ExpectedResult = 5)] // days of the Nameless One
+    [TestCase(1987, 6, 11, ExpectedResult = 30)]
+    public int GetDaysInMonth(int Year, int Month, int Era)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.GetDaysInMonth(Year, Month, Era);
+
+        // Assert
+        return result;
+    }
+
+    #endregion
+
+
+
+
+    #region Add Functions
+
+    [Test]
+    [TestCase("30.1.2000", 11, ExpectedResult = 2011)] // Leap year
+    [TestCase("31.1.2000", 12, ExpectedResult = 2012)] // Leap year
+    [TestCase("18.4.2022", 0, ExpectedResult = 2022)]
+    [TestCase("26.12.2005", -1, ExpectedResult = 2004)]
+    [TestCase("27.12.2005", 3, ExpectedResult = 2008)]
+    [TestCase("30.06.1988", 11, ExpectedResult = 1999)] // Leap year
+    public int AddYears(string Date, int Years)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.AddYears(time, Years);
+
+        // Assert
+        return result.Year;
+    }
+
+
+
+    [Test]
+    [TestCase("30.1.2000", 1, ExpectedResult = "01.3.2000")] // Leap year
+    [TestCase("30.1.2000", 2, ExpectedResult = "31.3.2000")] // Leap year
+    [TestCase("30.1.1999", 1, ExpectedResult = "01.3.1999")]
+    [TestCase("30.1.1999", 2, ExpectedResult = "31.3.1999")]
+    [TestCase("28.11.2005", 1, ExpectedResult = "28.12.2005")] // 2. Rahja -> 2. Namenlosen
+    [TestCase("26.12.2005", 1, ExpectedResult = "31.12.2005")] // 30. Rahja -> 5. Namelosen
+    [TestCase("26.12.2005", 2, ExpectedResult = "30.1.2006")]
+    [TestCase("27.12.2005", 1, ExpectedResult = "01.1.2006")]
+    [TestCase("27.12.2005", 2, ExpectedResult = "31.1.2006")]
+    [TestCase("30.06.1988", 2, ExpectedResult = "29.8.1988")] // Leap year
+    [TestCase("18.4.2022", 6, ExpectedResult = "15.10.2022")]
+    [TestCase("18.4.2022", 10, ExpectedResult = "18.1.2023")] // between years
+    public string AddMonths_LessThanYear(string Date, int Months)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.AddMonths(time, Months);
+
+        // Assert
+        return result.ToString("dd.M.yyyy");
+    }
+
+
+    [Test]
+    [TestCase("30.1.2000", 13, ExpectedResult = "30.1.2001")] // Leap year
+    [TestCase("31.1.2000", 13, ExpectedResult = "31.1.2001")] // Leap year
+    [TestCase("18.4.2022", 2 * 13, ExpectedResult = "18.4.2024")]
+    [TestCase("26.12.2005", 2 * 13, ExpectedResult = "26.12.2007")]
+    [TestCase("27.12.2005", 2 * 13, ExpectedResult = "27.12.2007")]
+    [TestCase("30.06.1988", 3 * 13, ExpectedResult = "30.6.1991")] // Leap year
+    public string AddMonths_13Months(string Date, int Months)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.AddMonths(time, Months);
+
+        // Assert
+        return result.ToString("dd.M.yyyy");
+    }
+
+
+    [Test]
+    [TestCase("1.1.2000", -1, ExpectedResult = "27.12.1999")] // between years, (Leap year)
+    [TestCase("2.1.2000", -1, ExpectedResult = "28.12.1999")] // between years, (Leap year)
+    [TestCase("31.1.2000", -2, ExpectedResult = "27.12.1999")] // between years, (Leap year)
+    [TestCase("30.1.2000", -2, ExpectedResult = "26.12.1999")] // between years, (Leap year)
+    [TestCase("26.12.2005", -1, ExpectedResult = "26.11.2005")]
+    [TestCase("26.12.2005", -2, ExpectedResult = "27.10.2005")] // 30. Rahja -> 30. Ingerimm
+    [TestCase("27.12.2005", -1, ExpectedResult = "27.11.2005")] // 1. Namenlos -> 1. Rahja
+    [TestCase("27.12.2005", -2, ExpectedResult = "28.10.2005")]
+    [TestCase("30.06.1988", -2, ExpectedResult = "01.5.1988")] // (Leap year)
+    [TestCase("18.4.2022", -6, ExpectedResult = "14.11.2021")]
+    [TestCase("18.4.2022", -10, ExpectedResult = "17.7.2021")] // between years
+    public string AddMonths_LessThanYearNegative(string Date, int Months)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        DateTime time;
+        Assume.That(DateTime.TryParse(Date, out time));
+
+        // Act
+        var result = bosparanCalendar.AddMonths(time, Months);
+
+        // Assert
+        return result.ToString("dd.M.yyyy");
+    }
+
+    #endregion
+
+
+
+    #region Leaping
+
+    [Test]
+    [TestCase(2000, 10, ExpectedResult = 0)] // Leap year
+    [TestCase(2000, 12, ExpectedResult = 0)] // Leap year
+    [TestCase(2022, 10, ExpectedResult = 0)]
+    [TestCase(2005, 12, ExpectedResult = 0)]
+    [TestCase(2005, 11, ExpectedResult = 0)]
+    [TestCase(1988, 11, ExpectedResult = 0)] // Leap year
+    public int GetLeapMonth_Always0(int Year, int Era)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.GetLeapMonth(Year, Era);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase(1932, ExpectedResult = false)]
+    [TestCase(2016, ExpectedResult = false)]
+    [TestCase(2028, ExpectedResult = false)]
+    [TestCase(2132, ExpectedResult = false)]
+    public bool IsLeapDay_EarthLeapDay_False(int Year, int Month = 2, int Day = 29)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.IsLeapDay(Year, Month, Day);
+
+        // Assert
+        return result;
+    }
+
+    [Test]
+    [TestCase(1900, ExpectedResult = false)]
+    [TestCase(2017, ExpectedResult = false)]
+    [TestCase(2018, ExpectedResult = false)]
+    [TestCase(2019, ExpectedResult = false)]
+    public bool IsLeapDay_NoEarthLeapDay_False(int Year, int Month = 2, int Day = 29)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.IsLeapDay(Year, Month, Day);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    public void IsLeapDay_VaryingEras_False(
+        [Values(10, 11, 12)] int Era, [Values(1900, 2016, 2019)] int Year)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        int Month = 2;
+        int Day = 29;
+
+        // Act
+        var result = bosparanCalendar.IsLeapDay(Year, Month, Day, Era);
+
+        // Assert
+        Assert.False(result);
+    }
+
+
+
+    [Test]
+    public void IsLeapMonth(
+        [Values(10, 11, 12)] int Era,
+        [Values(1900, 2016, 2019)] int Year,
+        [Values(1, 2, 12)] int Month)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.IsLeapMonth(Year, Month, Era);
+
+        // Assert
+        Assert.False(result);
+    }
+
+
+
+    [Test]
+    public void IsLeapYear(
+        [Values(10, 11, 12)] int Era,
+        [Values(1900, 2016, 2019)] int Year)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+
+        // Act
+        var result = bosparanCalendar.IsLeapYear(Year, Era);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    #endregion
+
+
+    [Test]
+    [TestCase(1045, 2, 1, ExpectedResult = "31.01.2022")]
+    [TestCase(1045, 2, 30, ExpectedResult = "01.03.2022")]
+    [TestCase(1047, 2, 1, ExpectedResult = "31.01.2024")] // earthen leap year
+    [TestCase(1047, 2, 30, ExpectedResult = "01.03.2024")] // earthen leap year
+    public string ToDateTime(int Year, int Month, int Day)
+    {
+        // Arrange
+        var bosparanCalendar = new BosparanCalendar();
+        int hour = 0;
+        int minute = 0;
+        int second = 0;
+        int millisecond = 0;
+        int era = 11;
+
+        // Act
+        var result = bosparanCalendar.ToDateTime(Year, Month, Day,
+            hour, minute, second, millisecond, era);
+
+        // Assert
+        return result.ToShortDateString();
+    }
+
+
+
+
+    [Test]
+    [TestCase(2000, 2, 29, 2009, 2, 27, ExpectedResult = 8)]
+    [TestCase(2000, 2, 29, 2009, 2, 28, ExpectedResult = 9)]
+    [TestCase(2000, 2, 29, 2009, 3, 1, ExpectedResult = 9)]
+
+    [TestCase(2000, 2, 28, 2011, 2, 28, ExpectedResult = 11)]
+    [TestCase(2000, 2, 28, 2011, 3, 1, ExpectedResult = 11)]
+
+    [TestCase(2000, 2, 29, 2011, 2, 27, ExpectedResult = 10)]
+    [TestCase(2000, 2, 29, 2011, 2, 28, ExpectedResult = 11)]
+    [TestCase(2000, 2, 29, 2011, 3, 1, ExpectedResult = 11)]
+
+    [TestCase(2000, 3, 1, 2011, 2, 27, ExpectedResult = 10)]
+    [TestCase(2000, 3, 1, 2011, 2, 28, ExpectedResult = 10)]
+    [TestCase(2000, 3, 1, 2011, 3, 1, ExpectedResult = 11)]
+    [TestCase(2022, 4, 14, 2025, 9, 28, ExpectedResult = 3)]//"28.9.2025"
+    public int DeltaInYears(int Year1, int Month1, int Day1, int Year2, int Month2, int Day2)
+    {
+        // Arrange
+        // Act
+        var result = BosparanCalendar.DeltaInYears(
+            new DateTime(Year1, Month1, Day1),
+            new DateTime(Year2, Month2, Day2));
+
+        // Assert
+        return result;
+    }
+}
+
+#pragma warning restore IDE0018 // Inlinevariablendeklaration
+
---
+++ dev/Calendar/BosparanCalendar.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace FateExplorer.Calendar;
+
+
+
+public class BosparanCalendar : System.Globalization.Calendar
+{
+	protected const int DaysInYear = 365;
+	protected const int DaysInMonth = 30;
+	protected const int NamelessDays = 5;
+	protected const int MonthsInYear = 13;
+	protected const int YearCorrectionFromGregorian = -977; // make this virtual to adapt all kinds of other calendars
+															// TODO: Add public virtual bool HasYear0 => true/false; // to adapt all kinds of other calendars
+
+	/// <summary>
+	/// Determines the difference of FULL years between two dates.
+	/// Leap days are ignored; the 29th of February is treated as if it was the 28th
+	/// (conforming with Aventurian calendars).
+	/// </summary>
+	/// <param name="Early">The earlier date</param>
+	/// <param name="Late">THe later date</param>
+	/// <returns>Difference in full years</returns>
+	public static int DeltaInYears(DateTime Early, DateTime Late)
+	{
+		if (Early > Late) throw new ArgumentException("'Late' must be greater or equal to 'Early'");
+
+		// Aventurian calendars do not have leap years
+		if (Early.Day == 29 && Early.Month == 2) Early = Early.AddDays(-1);
+		if (Late.Day == 29 && Late.Month == 2) Late = Late.AddDays(-1);
+
+		int Delta = Late.Year - Early.Year;
+
+		if (Late.Month < Early.Month || (Late.Month == Early.Month && Late.Day < Early.Day))
+			Delta--;
+
+		return Delta;
+	}
+
+
+	/// <inheritdoc/>
+	public override CalendarAlgorithmType AlgorithmType => CalendarAlgorithmType.SolarCalendar;
+
+
+	public override int GetMonth(DateTime time)
+			{
+		// Needed to determine leap years
+		GregorianCalendar EarthCalendar = new();
+
+		int Days = time.DayOfYear;
+		if (EarthCalendar.IsLeapYear(time.Year) && Days > 31 + 28) Days--;
+		return ((Days - 1) / DaysInMonth) + 1;
+
+	}
+
+	public override int GetYear(DateTime time) => time.Year + YearCorrectionFromGregorian; // TODO: use HasYear0
+
+	/// <inheritdoc/>
+	/// <remarks>The Karmakortheon is included in the previous era. At the moment 
+	/// the class assumes that we always play in the 11th age.</remarks>
+	public override int GetEra(DateTime time) => 11; // TODO: determine the era https://de.wiki-aventurica.de/wiki/Zeitalter
+
+	public override int GetDayOfMonth(DateTime time)
+			{
+		// Needed to determine leap years
+		GregorianCalendar EarthCalendar = new();
+
+		int Days = time.DayOfYear;
+		if (EarthCalendar.IsLeapYear(time.Year) && Days > 31 + 28) Days--;
+		return ((Days - 1) % 30) + 1;
+	}
+
+	/// <inheritdoc/>
+	public override DayOfWeek GetDayOfWeek(DateTime time)
+	{
+		// Needed to determine leap years
+		GregorianCalendar EarthCalendar = new();
+
+		// the 14.04.2022 on Earth is a Thursday; in Aventuria it is a Day of Praios (Sunday)
+		DateTime Reference = new (2022, 4, 14);
+	
+		DateTime Early = time <= Reference ? time : Reference;
+		DateTime Late  = time > Reference ? time : Reference;
+		int Years = DeltaInYears(Early, Late);
+
+		// skip leap days by using whole years
+		int Days = Years * DaysInYear; 
+		// add remaining interval
+		DateTime EarlyPlusYears = Early.AddYears(Years);
+		Days += (Late - EarlyPlusYears).Days;
+		// One leap day may be left, remove if necessary
+		if (EarthCalendar.IsLeapYear(EarlyPlusYears.Year) && EarlyPlusYears.DayOfYear < 31 + 29)
+			Days--;
+		else
+				{
+			if (EarthCalendar.IsLeapYear(Late.Year) && Late.DayOfYear > 31 + 28)
+				Days--;
+		}
+		
+		int Offset = Days % 7;
+
+		DayOfWeek Result;
+		if (time > Reference)
+			Result = (DayOfWeek)Offset;
+		else
+			Result = (DayOfWeek)(Offset == 0 ? 0 : 7 - Offset);
+
+		return Result;
+	}
+
+
+	/// <inheritdoc/>
+	public override int GetDayOfYear(DateTime time)
+	{
+		GregorianCalendar EarthCal = new();
+
+		if (EarthCal.IsLeapYear(time.Year))
+			if (time.DayOfYear > 31 + 28)
+				return time.DayOfYear - 1;
+		return time.DayOfYear;
+	}
+
+
+	public override int GetDaysInYear(int year) => DaysInYear;
+	public override int GetDaysInYear(int year, int era) => DaysInYear;
+
+	/// <inheritdoc/>
+	/// <remarks>Treats the days of the Nameless One as 13th month</remarks>
+	public override int GetMonthsInYear(int year, int era) => MonthsInYear;
+
+	/// <inheritdoc/>
+	/// <remarks>Accepts the days of the Nameless One as 13th month</remarks>
+	public override int GetDaysInMonth(int year, int month, int era)
+		{
+		if (month < 1 || month > 13) 
+			throw new ArgumentOutOfRangeException(nameof(month), month, "Calendar knows 12 months and 1 pseudo-month of 5 days of the Nameless One");
+		if (era < 1 || era > 12)
+			throw new ArgumentOutOfRangeException(nameof(era), era, "Calendar knows only eras from 1 to 12");
+
+		return (month <= 12 ? DaysInMonth : NamelessDays); // TODO check argument ranges
+	}
+    
+
+
+	public override DateTime AddYears(DateTime time, int years) => time.AddYears(years);
+
+
+	/// <inheritdoc/>
+	/// <remarks>Treats the days of the Nameless One as 13th month</remarks>
+	public override DateTime AddMonths(DateTime time, int months)
+	{
+		// Needed to determine leap years
+		GregorianCalendar EarthCalendar = new();
+
+		// 
+		int Month = GetMonth(time);
+		DateTime result = time.AddYears(months / MonthsInYear);
+		int Leftover = months % MonthsInYear;
+		if (Leftover == 0)
+			return result;
+		else
+		{
+			int Day = GetDayOfMonth(time);
+
+			int DaysToAdd;
+			if (Leftover > 0)
+			{
+				if (Month + Leftover < MonthsInYear)
+					DaysToAdd = DaysInMonth * Leftover;
+				else if (Month + Leftover == MonthsInYear) // new date lies in the days of the Nameless One
+					DaysToAdd = (DaysInMonth * Leftover) - Math.Max(0, Day - NamelessDays);
+				else // (Month + Leftover > 13)
+					DaysToAdd = DaysInMonth * (Leftover - 1) + NamelessDays;
+			}
+			else
+			{
+				if (Month + Leftover > 0) // still same year
+					DaysToAdd = DaysInMonth * Leftover;
+				else if (Month + Leftover == 0) // the 13th month of the previous year
+					DaysToAdd = (DaysInMonth * (Leftover+1)) - Math.Max(5, Day);
+				else // (Month + Leftover < 0) - previous year
+					DaysToAdd = DaysInMonth * (Leftover + 1) - NamelessDays;
+			}
+
+			if (EarthCalendar.IsLeapYear(result.Year))
+			{
+				if (result.DayOfYear < 31 + 29 && result.DayOfYear + DaysToAdd >= 31 + 29)
+					DaysToAdd += Leftover > 0 ? 1 : -1;
+				if (result.DayOfYear >= 31 + 29 && result.DayOfYear + DaysToAdd < 31 + 29)
+					DaysToAdd += Leftover > 0 ? 1 : -1;
+			}
+
+			return result.AddDays(DaysToAdd);
+		}
+	}
+
+	public override int GetLeapMonth(int year, int era) => 0;
+	public override bool IsLeapDay(int year, int month, int day) => false;
+	public override bool IsLeapDay(int year, int month, int day, int era) => false;
+	public override bool IsLeapMonth(int year, int month, int era) => false;
+	public override bool IsLeapYear(int year, int era) => false;
+
+
+	public override DateTime ToDateTime(int year, int month, int day, int hour, int minute, int second, int millisecond, int era)
+	{
+		if (month < 1 || month > 13)
+			throw new ArgumentOutOfRangeException(nameof(month), month, "Calendar knows 12 months and 1 pseudo-month of 5 days of the Nameless One");
+		if (era < 1 || era > 12)
+			throw new ArgumentOutOfRangeException(nameof(era), era, "Calendar knows only eras from 1 to 12");
+
+		GregorianCalendar EarthCalendar = new(); // Needed to determine leap years
+
+		int EarthYear = year - YearCorrectionFromGregorian;
+		DateTime result = new(EarthYear, 1, 1, hour, minute, second, millisecond, DateTimeKind.Local);
+		int DaysToAdd = (day - 1) + (month - 1) * DaysInMonth; // correct day by 1 because we already have the 1. of Jan.
+		// Add extra leap day that does not exist in Aventuria
+		if (EarthCalendar.IsLeapYear(EarthYear) && DaysToAdd >= 31+28) DaysToAdd++;
+
+		return result.AddDays(DaysToAdd);
+	}
+
+	public override int[] Eras => new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+}
+
