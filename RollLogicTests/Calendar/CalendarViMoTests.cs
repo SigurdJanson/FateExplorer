a0c0b80f23f26377a2eb79c32d5a3d3cae0c63cd
SigurdJanson <emailjs@web.de>
2022-04-24 23:14:44 +0200

    Added CalendarViMo to get access to the current date in the game play

---
+++ RollLogicTests/Calendar/CalendarViMoTests.cs
@@ -0,0 +1,146 @@
+﻿using FateExplorer.Calendar;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests.Calendar
+{
+    [TestFixture]
+    public class CalendarViMoTests
+    {
+        static IEnumerable<string> ValidStringDates()
+        {
+            yield return "4. Tsa 1028";
+            yield return "22. Boron 1045";
+            yield return "22. Bor 1045";
+            yield return "22. B 1045";
+            yield return "11 Rondra 998";
+            yield return "11 Rondra998";
+            yield return "    27. Efferd 12";
+            yield return " 27.Efferd 12  BF   ";
+            yield return " 27.Efferd 12  Bf   ";
+            yield return " 27.Efferd 12  bF   ";
+            yield return " 27.Efferd 12  bf   ";
+            yield return "4Tsa1028";
+            yield return "4. Tsa 0";
+            yield return "4. Tsa -976"; // earliest possible year
+        }
+
+        static IEnumerable<string> InvalidRangeStringDates()
+        {
+            yield return "4. Tsa -977"; // latest invalid year
+            yield return " 31. Efferd 12 BF";
+            yield return " 0. Namenlos 12";
+        }
+
+        private MockRepository mockRepository;
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+        }
+
+
+        private CalendarViMo CreateCalendarViMo()
+        {
+            return new CalendarViMo();
+        }
+
+
+
+        [Test]
+        public void GotoTomorrow_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+
+            // Act
+            calendarViMo.GotoTomorrow();
+
+            // Assert
+            //Assert.AreEqual();
+            this.mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void GotoYesterday_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+
+            // Act
+            calendarViMo.GotoYesterday();
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void GotoEarthDate_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+
+            // Act
+            calendarViMo.GotoEarthDate();
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+
+
+
+        [Test]
+        [TestCaseSource(nameof(ValidStringDates))]
+        public void Parse_ValidDateTime(string dateStr)
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+            DateTime result = DateTime.MinValue;
+
+            // Act
+            Assert.DoesNotThrow(() => result = calendarViMo.Parse(dateStr));
+
+            // Assert
+            Assert.Greater(result, DateTime.MinValue);
+            mockRepository.VerifyAll();
+        }
+
+
+        [Test]
+        [TestCaseSource(nameof(InvalidRangeStringDates))]
+        public void Parse_InvalidRange_Exception(string dateStr)
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+            DateTime result = DateTime.MinValue;
+
+            // Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => result = calendarViMo.Parse(dateStr));
+
+            // Assert
+            mockRepository.VerifyAll();
+        }
+
+        [Test]
+        [TestCaseSource(nameof(InvalidRangeStringDates))]
+        public void Parse_InvalidMonthName_Exception(string dateStr)
+        {
+            // Arrange
+            var calendarViMo = this.CreateCalendarViMo();
+            DateTime result = DateTime.MinValue;
+
+            // Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => result = calendarViMo.Parse(dateStr));
+
+            // Assert
+            mockRepository.VerifyAll();
+        }
+    }
+}
--- dev/Calendar/CalendarViMo.cs
+++ dev/Calendar/CalendarViMo.cs
@@ -1,5 +1,6 @@
 ﻿using MudBlazor;
 using System;
+using System.Text.RegularExpressions;
 
 namespace FateExplorer.Calendar;
 
@@ -10,8 +11,9 @@ namespace FateExplorer.Calendar;
 /// <remarks>In this early version the CalendarViMo can only be used with reckoning of Bosparan's Fall</remarks>
 public class CalendarViMo
 {
-    private readonly string[] MonthAbbr = new[] { "Praios", "Rondra", "Efferd", "Travia", "Boron", "Hesinde", "Firun", "Tsa", "Phex", "Peraine", "Ingerimm", "Rahja", "Namenloser" };
-    private readonly string[] WeekDay = new[] { "Praiostag", "Rondratag", "Feuertag", "Wassertag", "Windstag", "Erdtag", "Markttag" };
+    private readonly string[] MonthName = new[] { "Praios", "Rondra", "Efferd", "Travia", "Boron", "Hesinde", "Firun", "Tsa", "Phex", "Peraine", "Ingerimm", "Rahja", "Namenloser" };
+    private readonly string[] MonthAbbr = new[] { "Pra", "Ron", "Eff", "Tra", "Bor", "Hes", "Fir", "Tsa", "Phx", "Per", "Ing", "Rah", "Nal" };
+    private readonly string[] WeekDayName = new[] { "Praiostag", "Rondratag", "Feuertag", "Wassertag", "Windstag", "Erdtag", "Markttag" };
     private readonly string[] WeekDayAbbr = new[] { "Pr", "Ro", "Fe", "Wa", "Wi", "Er", "Ma" };
     private BosparanCalendar Calendar { get; set; }
     private DateTime CurrentDate { get; set; }
@@ -24,13 +26,16 @@ public class CalendarViMo
         Calendar = new();
     }
 
+    public const string DateRegex = @"^\s*(?<day>\d{1,2})\.?\s*(?<month>[A-Z]*)\s*(?<year>-?\d{1,4})\s*(?<reck>[A-Z]*)\s*$";
+
+
     public string WeekdayShort => WeekDayAbbr[(int)Calendar.GetDayOfWeek(EffectiveDate)];
-    public string Weekday => WeekDay[(int)Calendar.GetDayOfWeek(EffectiveDate)];
+    public string Weekday => WeekDayName[(int)Calendar.GetDayOfWeek(EffectiveDate)];
 
     public int DayOfMonth => Calendar.GetDayOfMonth(EffectiveDate);
 
-    public string Month => MonthAbbr[Calendar.GetMonth(EffectiveDate) - 1];
-    public string MonthIcon => IconsFE.Concat(MonthAbbr[Calendar.GetMonth(EffectiveDate) - 1]);
+    public string Month => MonthName[Calendar.GetMonth(EffectiveDate) - 1];
+    public string MonthIcon => IconsFE.Concat(MonthName[Calendar.GetMonth(EffectiveDate) - 1]);
     public int MonthNr => Calendar.GetMonth(EffectiveDate);
 
     public int Year => Calendar.GetYear(EffectiveDate);
@@ -49,7 +54,52 @@ public class CalendarViMo
 
 
     public void GotoTomorrow() => EffectiveDate = EffectiveDate.AddDays(1);
+
     public void GotoYesterday() => EffectiveDate = EffectiveDate.AddDays(-1);
 
     public void GotoEarthDate() => EffectiveDate = DateTime.Now;
+
+    public void GotoDate(DateTime date) => EffectiveDate = date;
+
+
+    // https://stackoverflow.com/questions/56065683/regex-for-matching-dates-month-day-year-or-m-d-yy?msclkid=f2b2cd08c3af11ec8681f35279f46fe3
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inlinevariablendeklaration", Justification = "Readibility")]
+    public DateTime Parse(string dateStr)
+    {
+        //string pattern = @"^\s * (? 'day'\d{ 1,2})\.?\s * (? 'month'[A - Za - z] *)\s * (? 'year'\d{ 1,4})\s * (? 'Reck'[A - Za - z] *)\s *$";
+        const string DayName = "day";
+        const string MonthName = "month";
+        const string YearName = "year";
+        const string ReckoningName = "reck";
+
+        Regex rgx = new(DateRegex, RegexOptions.IgnoreCase);
+        Match match = rgx.Match(dateStr);
+        if (!match.Success)
+            throw new FormatException($"String {dateStr} could not be interpreted as date");
+
+        string Reckoning = match.Groups[ReckoningName].Value;
+        if (!string.IsNullOrEmpty(Reckoning))
+            if (!Reckoning.Equals("bf", StringComparison.InvariantCultureIgnoreCase))
+                throw new FormatException($"String {dateStr} could not be interpreted as date. Reckoning not recognized.");
+
+        int Day = int.Parse(match.Groups[DayName].Value);
+        int Year = int.Parse(match.Groups[YearName].Value);
+
+        int Month;
+        string MonthStr = match.Groups[MonthName].Value;
+        if (!int.TryParse(MonthStr, out Month))
+        {
+            Month = 1;
+            foreach (var m in this.MonthName)
+                if (!m.StartsWith(MonthStr, StringComparison.InvariantCultureIgnoreCase))
+                    ++Month;
+                else
+                    break;
+            if (Month > MonthName.Length)
+                throw new FormatException($"String {dateStr} could not be interpreted as date. Month could not be matched.");
+        }
+
+        return Calendar.ToDateTime(Year, Month, Day, 0, 0, 0, 0);
+    }
+    
 }
