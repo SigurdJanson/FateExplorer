cb915c57c3c437e60ee87a5cfe572d7084af1761
SigurdJanson <emailjs@web.de>
2022-04-25 19:28:05 +0200

    Added access to Nameless One icon; added access to moon phases

--- dev/Shared/IconsFE.cs
+++ dev/Shared/IconsFE.cs
@@ -12,16 +12,19 @@ namespace MudBlazor
 
         // ICONS
 
-        public static string Success => Concat(nameof(Success));
+        public static string Success => Ligaturisation + Prefix + nameof(Success);
         public static string Botch => Ligaturisation + Prefix + nameof(Botch);
         public static string PotionArcane => Ligaturisation + Prefix + nameof(PotionArcane);
         public static string PotionLife => Ligaturisation + Prefix + nameof(PotionLife);
+
+        public static string D6(int d) => Concat($"{nameof(D6)}_{d}");
         public static string D6_1 => Ligaturisation + Prefix + nameof(D6_1);
         public static string D6_2 => Ligaturisation + Prefix + nameof(D6_2);
         public static string D6_3 => Ligaturisation + Prefix + nameof(D6_3);
         public static string D6_4 => Ligaturisation + Prefix + nameof(D6_4);
         public static string D6_5 => Ligaturisation + Prefix + nameof(D6_5);
         public static string D6_6 => Ligaturisation + Prefix + nameof(D6_6);
+
         public static string LocalMallClear => Ligaturisation + Prefix + nameof(LocalMallClear);
         public static string Ulisses => Ligaturisation + Prefix + nameof(Ulisses);
         public static string BackHandLeft => Ligaturisation + Prefix + nameof(BackHandLeft);
@@ -40,7 +43,15 @@ namespace MudBlazor
         public static string Rondra => Ligaturisation + Prefix + nameof(Rondra);
         public static string Travia => Ligaturisation + Prefix + nameof(Travia);
         public static string Tsa => Ligaturisation + Prefix + nameof(Tsa);
-        //public static string Nameless => Ligaturisation + Prefix + nameof(Nameless);
+        public static string NamelessOne => Ligaturisation + Prefix + nameof(NamelessOne);
 
+        // MADAS MARK - MOON PHASES
+        /// <summary>
+        /// Get icons by moon phase
+        /// </summary>
+        /// <param name="phase">1-12, 1 is new moon (dead mada), 4 is half, 7 is full moon (wheel),, 
+        /// 10 is half, 12 the phase before new moon</param>
+        /// <returns></returns>
+        public static string Moon(int phase) => Concat($"{nameof(Moon)}Phase{phase}");
     }
 }
