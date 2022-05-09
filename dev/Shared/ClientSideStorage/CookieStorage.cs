5757b032954f625778cfd7026d70c43607943cd2
SigurdJanson <emailjs@web.de>
2022-05-09 21:11:05 +0200

    Refactor: comments, code reorg; checking for non-ascii characters

--- dev/Shared/ClientSideStorage/CookieStorage.cs
+++ dev/Shared/ClientSideStorage/CookieStorage.cs
@@ -24,52 +24,59 @@ public class CookieStorage : IClientSideStorage
         ExpireDays = DefaultExpirationDays;
     }
 
-
     /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="key"></param>
-    /// <param name="value"></param>
-    /// <param name="days">Set the number of days the cookie should survive. 
-    /// If <c>null</c> use the default <see cref="DefaultExpirationDays"/>.</param>
-    /// <returns></returns>
-    public async Task SetValue(string key, string value, int? days = null)
-    {
-        var curExp = (days != null) ? (days > 0 ? DateToUTC(days.Value) : "") : expires;
-        await SetCookie($"{key}={value}; expires={curExp}; path=/");
-    }
-
-    public async Task<string> GetValue(string key, string def = "")
-    {
-        var cValue = await GetCookie();
-        if (string.IsNullOrEmpty(cValue)) return def;
-
-        var vals = cValue.Split(';');
-        foreach (var val in vals)
-            if (!string.IsNullOrEmpty(val) && val.IndexOf('=') > 0)
-                if (val.Substring(0, val.IndexOf('=')).Trim().Equals(key, StringComparison.OrdinalIgnoreCase))
-                    return val.Substring(val.IndexOf('=') + 1);
-        return def;
-    }
-
-    private async Task SetCookie(string value)
-    {
-        await JSRuntime.InvokeVoidAsync("eval", $"document.cookie = \"{value}\"");
-    }
-
-    private async Task<string> GetCookie()
-    {
-        return await JSRuntime.InvokeAsync<string>("eval", $"document.cookie");
-    }
-
-    /// <summary>
-    /// Set the lifetime of the cookie by the number of days it shall survive.
+    /// Set the default lifetime of stored data by the number of days it shall survive.
     /// </summary>
     public int ExpireDays
     {
         set => expires = DateToUTC(value);
     }
 
+
+    /// <param name="days">Set the number of days the cookie should survive. 
+    /// If <c>null</c> use the default <see cref="DefaultExpirationDays"/>.</param>
+    /// <exception cref="ArgumentException"/>
+    /// <inheritdoc/>
+    public async Task SetValue(string key, string value, int? days = null)
+    {
+        if (HasNonASCIIChars(key)) 
+            throw new ArgumentException("Only ascii characters are allowed in key string", nameof(key));
+        if (HasNonASCIIChars(value)) 
+            throw new ArgumentException("Only ascii characters are allowed in value string", nameof(value));
+
+        var curExp = (days != null) ? (days > 0 ? DateToUTC(days.Value) : "") : expires;
+        await SetCookie($"{key}={value}; expires={curExp}; path=/");
+    }
+
+
+    public async Task<string> GetValue(string key, string defaultVal = "")
+    {
+        var cValue = await GetCookie();
+        if (string.IsNullOrEmpty(cValue)) return defaultVal;
+
+        var vals = cValue.Split(';');
+        foreach (var val in vals)
+            if (!string.IsNullOrEmpty(val) && val.IndexOf('=') > 0)
+                if (val[..val.IndexOf('=')].Trim().Equals(key, StringComparison.OrdinalIgnoreCase))
+                    return val[(val.IndexOf('=') + 1)..];
+        return defaultVal;
+    }
+
+
+    // HELPERS
+
+    private async Task SetCookie(string value)
+        => await JSRuntime.InvokeVoidAsync("eval", $"document.cookie = \"{value}\"");
+
+    private async Task<string> GetCookie()
+        => await JSRuntime.InvokeAsync<string>("eval", $"document.cookie");
+
+
     private static string DateToUTC(int days) => DateTime.Now.AddDays(days).ToUniversalTime().ToString("R");
+
+    private static bool HasNonASCIIChars(string str)
+    {
+        return System.Text.Encoding.UTF8.GetByteCount(str) != str.Length;
+    }
 }
 
--- dev/Shared/ClientSideStorage/IClientSideStorage.cs
+++ dev/Shared/ClientSideStorage/IClientSideStorage.cs
@@ -4,6 +4,23 @@ namespace FateExplorer.Shared.ClientSideStorage;
 
 public interface IClientSideStorage
 {
+    /// <summary>
+    /// Write a value into storage
+    /// </summary>
+    /// <param name="key">A key to identify (and later retrieve) the stored data.</param>
+    /// <param name="value">The value to store.</param>
+    /// <param name="days">Set an expiration date as number of days the cookie should survive. 
+    /// If <c>null</c>, provide a default.</param>
+    /// <returns></returns>
     public Task SetValue(string key, string value, int? days = null);
-    public Task<string> GetValue(string key, string def = "");
+
+
+    /// <summary>
+    /// Retrieve a value from storage
+    /// </summary>
+    /// <param name="key">A key to identify and retrieve the stored data.</param>
+    /// <param name="defaultVal">The default value to be returned if <see cref="key"/> is 
+    /// not found. Default of the default is an empty string.</param>
+    /// <returns>The stored value as string</returns>
+    public Task<string> GetValue(string key, string defaultVal = "");
 }
