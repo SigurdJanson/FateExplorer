f1bed4e73aeb95d0b1a1d3d8cb0e5f516ed2be18
SigurdJanson <emailjs@web.de>
2022-05-11 22:26:56 +0200

    Moved the functionality that handles client side storage to it's own project to handle dependencies. A dependency from one WASM to another did not work.

---
+++ LibClientSideStorage/IClientSideStorage.cs
@@ -0,0 +1,61 @@
+﻿namespace FateExplorer.Shared.ClientSideStorage;
+
+public interface IClientSideStorage
+{
+    /// <summary>
+    /// Write a value into storage.
+    /// </summary>
+    /// <param name="key">A key to identify (and later retrieve) the stored data.</param>
+    /// <param name="value">The value to store.</param>
+    /// <param name="days">Set an expiration date as number of days the cookie should survive. 
+    /// If <c>null</c>, provide a default.</param>
+    /// <returns>-</returns>
+    public Task Store(string key, string value, int? days = null);
+
+
+
+    /// <summary>
+    /// Put an object into storage.
+    /// </summary>
+    /// <typeparam name="T">Any serialisable type; preferably a mere data transfer object.</typeparam>
+    /// <inheritdoc cref = "Store" />
+    public Task Store<T>(string key, T value, int? days = null);
+
+
+
+    /// <summary>
+    /// Retrieve a single value from storage.
+    /// </summary>
+    /// <param name="key">A key to identify and retrieve the stored data.</param>
+    /// <param name="defaultVal">The default value to be returned if <see cref="key"/> is 
+    /// not found. Default of the default is an empty string.</param>
+    /// <returns>The stored value as string</returns>
+    public Task<string> Retrieve(string key, string? defaultVal = "");
+
+
+
+    /// <summary>
+    /// Retrieve an object from storage.
+    /// </summary>
+    /// <typeparam name="T">Any serialisable type; preferably a mere data transfer object.</typeparam>
+    /// <inheritdoc cref = "Retrieve" />
+    public Task<T?> Retrieve<T>(string key, T? defaultVal);
+
+
+
+    /// <summary>
+    /// Remove value from storage
+    /// </summary>
+    /// <param name="key">The key to identify the value</param>
+    /// <returns></returns>
+    public Task Delete(string key);
+
+
+
+    /// <summary>
+    /// Check if value exists
+    /// </summary>
+    /// <param name="key">A key to identify the stored data.</param>
+    /// <returns><c>true</c> if it exists; otherwise <c>false</c>.</returns>
+    public Task<bool> Exists(string key);
+}
