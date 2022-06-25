b2af5d99596cd0600de191c21b28a61dd9aae4e4
SigurdJanson <emailjs@web.de>
2022-06-25 17:47:27 +0200

    Tests for `CurrencyM`

--- dev/Shop/CurrencyM.cs
+++ dev/Shop/CurrencyM.cs
@@ -1,24 +1,23 @@
 ﻿using FateExplorer.GameData;
 
-namespace FateExplorer.Shop
+namespace FateExplorer.Shop;
+
+public class CurrencyM
 {
-    public class CurrencyM
+    public CurrencyM(CurrencyDbEntry currencyDbEntry)
     {
-        public CurrencyM(CurrencyDbEntry currencyDbEntry)
-        {
-            this.Id = currencyDbEntry.Id;
-            this.Origin = currencyDbEntry.Origin;
-            this.Name = currencyDbEntry.Name;
-            this.Rate = currencyDbEntry.Rate;
-        }
-
-
-        public string Id { get; }
-
-        public string Origin { get; }
-
-        public string Name { get; }
-
-        public double Rate { get; }
+        this.Id = currencyDbEntry.Id;
+        this.Origin = currencyDbEntry.Origin;
+        this.Name = currencyDbEntry.Name;
+        this.Rate = currencyDbEntry.Rate;
     }
+
+
+    public string Id { get; }
+
+    public string Origin { get; }
+
+    public string Name { get; }
+
+    public double Rate { get; }
 }
