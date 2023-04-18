75d822f5cc385ce32e53e28061f5ca4c3c81608d
Jan <59577271+SigurdJanson@users.noreply.github.com>
2023-04-18 22:38:24 +0200

    Renamed folder; added BaseContextM
    BaseContextM offers a plain context that only recognizes a mere free modifier

---
+++ dev/RollLogic/Context/BaseContextM.cs
@@ -0,0 +1,46 @@
+﻿using FateExplorer.Shared;
+using System;
+
+namespace FateExplorer.RollLogic;
+
+
+public class BaseContextM : ICheckContext
+{
+    /*
+     * Implement `IStateContainer`
+     */
+    public event Action OnStateChanged;
+
+    private void NotifyStateChange() => OnStateChanged?.Invoke();
+
+
+    private const int FreeModifierDefault = 0;
+    private int freeModifier = FreeModifierDefault;
+    /// <summary>
+    /// An additional additive modifier to be used in addition to the context itself.
+    /// </summary>
+    public int FreeModifier
+    {
+        get => freeModifier;
+        set
+        {
+            if (freeModifier != value)
+            {
+                freeModifier = value;
+                NotifyStateChange();
+            }
+        }
+    }
+
+
+    public int ApplyTotalMod(int before, Check.Combat action)
+    {
+        return before + FreeModifier;
+    }
+
+    public Modifier GetTotalMod(int before, Check.Combat action)
+    {
+        return new Modifier(FreeModifier);
+    }
+}
+
