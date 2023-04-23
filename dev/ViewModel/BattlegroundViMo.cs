d7bd14f1c9140d60a7a8c55374a3f31478f29822
Jan <59577271+SigurdJanson@users.noreply.github.com>
2023-04-23 14:43:17 +0200

    Allow get and set for context püroperties

--- dev/ViewModel/BattlegroundViMo.cs
+++ dev/ViewModel/BattlegroundViMo.cs
@@ -8,16 +8,32 @@ public class BattlegroundViMo
 {
     private BattlegroundM Battleground { get; set; }
 
-    public int FreeModifier => Battleground.FreeModifier;
-    public WeaponsRange Distance => Battleground.Distance;
-    public Vision View => Battleground.Visibility;
-    public UnderWater Water => Battleground.Water;
-    public bool CrampedSpace => Battleground.CrampedSpace;
+    public int FreeModifier { 
+        get => Battleground.FreeModifier; set => Battleground.FreeModifier = value; 
+    }
+    public WeaponsRange Distance { 
+        get => Battleground.Distance; set => Battleground.Distance = value; 
+    }
+    public Vision View {
+        get => Battleground.Visibility; set => Battleground.Visibility = value;
+    }
+    public UnderWater Water {
+        get => Battleground.Water; set => Battleground.Water = value;
+    }
+    public bool CrampedSpace {
+        get => Battleground.CrampedSpace; set => Battleground.CrampedSpace = value;
+    }
     public Movement Moving => Battleground.Moving;
-    public Movement EnemyMoving => Battleground.EnemyMoving;
+    public Movement EnemyMoving {
+        get => Battleground.EnemyMoving; set => Battleground.EnemyMoving = value;
+    }
     public WeaponsReach EnemyReach => Battleground.EnemyReach;
-    public EnemySize SizeOfEnemy => Battleground.SizeOfEnemy;
-    public bool EnemyEvasive => Battleground.EnemyEvasive;
+    public EnemySize SizeOfEnemy {
+        get => Battleground.SizeOfEnemy; set => Battleground.SizeOfEnemy = value;
+    }
+    public bool EnemyEvasive {
+        get => Battleground.EnemyEvasive; set => Battleground.EnemyEvasive = value;
+    }
 
     public BattlegroundViMo(bool useMainHand, WeaponViMo mainWeapon, WeaponViMo offWeapon)
     {
