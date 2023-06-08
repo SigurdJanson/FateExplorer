d43987a58cec11c59bc64fa6211a58b64dfa494f
Jan <59577271+SigurdJanson@users.noreply.github.com>
2023-06-08 21:36:43 +0200

    Added ViewModel for Praises and Insults

---
+++ RollLogicTests/ViewModel/PraiseViMoTests.cs
@@ -0,0 +1,109 @@
+﻿using FateExplorer.GameData;
+using FateExplorer.Shared;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace FateExplorer.ViewModel.Tests;
+
+
+
+[TestFixture()]
+public class PraiseViMoTests
+{
+    private MockRepository mockRepository;
+    private Mock<IGameDataService> gameDataService;
+    private PraiseOrInsultDB Data;
+
+    private PraiseViMo praise;
+
+    private static readonly string[] FateQuoteData = new[] { "FateQuote0", "FateQuote1" };
+    private static readonly PraiseOrInsultEntry[] PraiseData = new[] 
+    { 
+        new PraiseOrInsultEntry() { Text = "Praise0", Scope = 255 }, 
+        new PraiseOrInsultEntry() { Text = "Praise1", Scope = 1 } 
+    };
+    private static readonly PraiseOrInsultEntry[] InsultData = new[]
+    {
+        new PraiseOrInsultEntry() { Text = "Insult0", Scope = 255 },
+        new PraiseOrInsultEntry() { Text = "Insult1", Scope = 1 }
+    };
+    private static readonly string[] PraisePhraseData = new[] { "Phrase0 {0}", "Phrase1 {0}" };
+    private static readonly string[] AdjectiveData = new[] { "Adj0", "Adj1" };
+
+
+    [SetUp]
+    public void Setup()
+    {
+        mockRepository = new(MockBehavior.Strict);
+        gameDataService = mockRepository.Create<IGameDataService>();
+
+        Data = new()
+        {
+            FateQuote = new[] { "FateQuote0", "FateQuote1" },
+            Praise = PraiseData, Insult = InsultData,
+            PraisePhrase = PraisePhraseData,
+            PositiveAdjective = AdjectiveData
+        };
+        gameDataService.SetupGet(s => s.PraiseOrInsult).Returns(Data);
+
+        praise = new(gameDataService.Object);
+    }
+
+
+    [Test, Retry(7)]
+    public void GetFateQuoteTest()
+    {
+        // Arrange
+        // Act
+        var result = praise.GetFateQuote();
+
+        // Assert
+        Assert.That(result, Is.AnyOf(FateQuoteData));
+        mockRepository.Verify();
+    }
+
+
+
+    [Test, Retry(7)]
+    [TestCase(PraiseViMo.Praise, false)]
+    [TestCase(PraiseViMo.Insult, false)]
+    [TestCase(PraiseViMo.Praise, true)]
+    [TestCase(PraiseViMo.Insult, true)]
+    public void GiveTest(int Tone, bool isCombat)
+    {
+        // Arrange
+        Check check = new(Check.Roll.Dodge);
+
+        // Act
+        var result = praise.Give(Tone, check);
+        
+        // Assert
+        if (Tone == PraiseViMo.Praise)
+        {
+            if (isCombat)
+                Assert.That(result, Is.AnyOf(new[] { Array.Find(PraiseData, i => i.Scope == 255).Text, "Phrase0 Adj0", "Phrase1 Adj1" }));
+            else
+                Assert.That(result, Is.AnyOf(new[] { PraiseData[0].Text, PraiseData[1].Text, "Phrase0 Adj0", "Phrase1 Adj1" }));
+        }
+        else if (Tone == PraiseViMo.Insult)
+        {
+            if (isCombat)
+            {
+                var Expected = Array.Find(InsultData, i => i.Scope == 255).Text;
+                Console.WriteLine($"Expected: {Expected}");
+                Assert.That(result, Is.EqualTo(Expected));
+            }
+            else
+            {
+                var Expected = InsultData.Select(i => i.Text).ToArray();
+                Console.WriteLine($"Expected: {Expected}");
+                Assert.That(result, Is.AnyOf(Expected));
+            }
+        }
+        else
+            Assert.Fail();
+        mockRepository.Verify();
+    }
+}
---
+++ dev/ViewModel/PraiseViMo.cs
@@ -0,0 +1,64 @@
+﻿using FateExplorer.GameData;
+using FateExplorer.Shared;
+using System;
+using System.Linq;
+
+namespace FateExplorer.ViewModel;
+
+public class PraiseViMo
+{
+    public const int Praise = 0;
+    public const int Insult = 1;
+
+    protected PraiseOrInsultDB Data;
+    protected Random Rnd;
+
+    public PraiseViMo(IGameDataService gameData)
+    {
+        this.Data = gameData.PraiseOrInsult ?? throw new ArgumentNullException(nameof(gameData));
+        Rnd = new();
+    }
+
+    public string GetFateQuote()
+    {
+        int i = Rnd.Next(Data.FateQuote.Length);
+        return Data.FateQuote[i];
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="tone"></param>
+    /// <param name="check"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public string Give(int tone, Check check) 
+    {
+        int Scope = check.IsCombat ? 1 : 255;
+        if (tone == Praise) 
+        {
+            if (Rnd.Next(2) > 0) // predefined phrase or one created from adjective
+            {   // Create a sentence using a praise adjective
+                int i = Rnd.Next(Data.PraisePhrase.Length);
+                string Sentence = Data.PraisePhrase.ElementAt(i);
+                i = Rnd.Next(Data.PositiveAdjective.Length);
+                string Adjective = Data.PositiveAdjective[i];
+                return string.Format(Sentence, Adjective);
+            }
+            else
+            {   // Create a predefined sentence
+                var Sub = Data.Praise.Where(t => (t.Scope | Scope) > 0);
+                int i = Rnd.Next(Sub.Count());
+                return Sub.ElementAt(i).Text;
+            }
+        }
+        if (tone == Insult)
+        {
+            var Sub = Data.Insult.Where(t => (t.Scope | Scope) > 0);
+            int i = Rnd.Next(Sub.Count());
+            return Data.Insult.ElementAt(i).Text;
+        }
+        throw new ArgumentException("Unknown form of praise or insult");
+    }
+}
