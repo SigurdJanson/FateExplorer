af74af0e45a62d25fa00a4e806a9e56705be102b
Jan <59577271+SigurdJanson@users.noreply.github.com>
2023-07-01 12:59:17 +0200

    Added class to de-serialise the dishes and inn names

---
+++ dev/Inn/InnDataM.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace FateExplorer.Inn;
+
+
+
+
+public class InnDataM
+{
+    /* 
+     * Aventurian Dishes
+     */
+    [JsonPropertyName("unit"), JsonRequired]
+    public string[] Unit { get; set; }
+
+    [JsonPropertyName("categories"), JsonRequired]
+    public Dictionary<int, string> Category { get; set; }
+
+    [JsonPropertyName("dish"), JsonRequired]
+    public List<InnDishM> Dish { get; set; }
+
+    /* 
+     * Inn Names
+     */
+    [JsonPropertyName("inns"), JsonRequired]
+    public InnNameRecord InnNames { get; set; }
+
+    public record InnNameRecord
+    {
+        [JsonPropertyName("qualifier"), JsonRequired]
+        public InnQualifierM[] Qualifier { get; set; }
+
+        [JsonPropertyName("namebase"), JsonRequired]
+        public InnNameBaseM[] NameBase { get; set; }
+
+        [JsonPropertyName("name"), JsonRequired]
+        public InnNameM[] FullName { get; set; }
+    }
+
+    public InnQualifierM[] Qualifier => InnNames.Qualifier;
+    public InnNameBaseM[] NameBase => InnNames.NameBase;
+    public InnNameM[] FullName => InnNames.FullName;
+}
