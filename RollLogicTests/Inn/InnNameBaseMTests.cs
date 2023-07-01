c2b61c677527a551125cfd132c7d68debed5e18b
Jan <59577271+SigurdJanson@users.noreply.github.com>
2023-07-01 11:14:00 +0200

    Added class to provide the noun of a generated inn name

---
+++ RollLogicTests/Inn/InnNameBaseMTests.cs
@@ -0,0 +1,63 @@
+﻿using FateExplorer.Inn;
+using NUnit.Framework;
+
+namespace UnitTests.Inn;
+
+[TestFixture]
+public class InnNameBaseMTests
+{
+    private InnNameBaseM _innNameBaseM;
+
+    [SetUp]
+    public void Setup()
+    {
+        _innNameBaseM = new InnNameBaseM();
+    }
+
+    [Test]
+    public void PluralIsSuffix_PluralNotNullAndSuffix_ReturnsTrue()
+    {
+        _innNameBaseM.Plural = "-s";
+        Assert.IsTrue(_innNameBaseM.PluralIsSuffix);
+    }
+
+    [Test]
+    public void PluralIsSuffix_PluralNotNullAndNotSuffix_ReturnFalse()
+    {
+        _innNameBaseM.Plural = "Häuser";
+        Assert.IsFalse(_innNameBaseM.PluralIsSuffix);
+    }
+
+    [Test]
+    public void PluralIsSuffix_PluralIsNull_ReturnsFalse()
+    {
+        _innNameBaseM.Plural = null;
+        Assert.IsFalse(_innNameBaseM.PluralIsSuffix);
+    }
+
+
+
+    [Test]
+    public void Plural_PluralIsSuffix_ReturnsConcat()
+    {
+        _innNameBaseM.Singular = "Inn";
+        _innNameBaseM.Plural = "-s";
+        Assert.AreEqual("Inns", _innNameBaseM.Plural);
+    }
+
+    [Test]
+    public void Plural_WhenPluralIsNotSuffix_ReturnsPlural()
+    {
+        _innNameBaseM.Singular = "Mouse";
+        _innNameBaseM.Plural = "Mice";
+        Assert.AreEqual("Mice", _innNameBaseM.Plural);
+    }
+
+    [Test]
+    public void Plural_WhenPluralIsNull_ReturnsSingular()
+    {
+        _innNameBaseM.Singular = "Keiler";
+        _innNameBaseM.Plural = null;
+        Assert.AreEqual("Keiler", _innNameBaseM.Plural);
+    }
+}
---
+++ dev/Inn/InnNameBaseM.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+using System;
+
+namespace FateExplorer.Inn;
+
+public class InnNameBaseM : InnNamesSharedM
+{
+    private string _Plural; // field to suport the `Plural` property
+
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Singular { get; set; }
+
+    [JsonPropertyName("plural")]
+    public string Plural 
+    { 
+        get
+        {
+            if (PluralIsSuffix)
+                return $"{Singular}{_Plural[1..]}";
+            else if (string.IsNullOrWhiteSpace(_Plural))
+                return Singular;
+            return _Plural;
+        }
+        set => _Plural = value;
+    }
+
+    [JsonPropertyName("prefix")]
+    public string Prefix { get; set; }
+
+
+
+    /// <summary>
+    /// Tells if the plural form of this base name must be appended to the singular or if it is a whole word.
+    /// </summary>
+    public bool PluralIsSuffix => _Plural?.StartsWith('-') ?? false;
+}
