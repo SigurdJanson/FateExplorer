481e0637510f635c7cb332f06de7432a1fe44a58
Jan <59577271+SigurdJanson@users.noreply.github.com>
2025-08-16 23:09:31 +0200

    Added class to fix magic strings
    ...and removed unneeded key strings in the process

---
+++ dev/Shared/KeyCode.cs
@@ -0,0 +1,23 @@
+﻿namespace FateExplorer.Shared
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>More key code at https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values</remarks>
+    public class KeyCode
+    {
+        public const string Escape = "Escape";
+
+        // Whitespace keys
+        public const string Enter = "Enter";
+        public const string Tab = "Tab";
+        public const string Space = " ";
+
+        //
+        public const string Unidentified = "Unidentified";
+
+        // Letters
+        public const string A = "A";
+    }
+}
