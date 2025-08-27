1838f82d2a10f0e1de8b649e1653591256cc1801
Jan <59577271+SigurdJanson@users.noreply.github.com>
2025-08-28 00:00:41 +0200

    After .NET update

--- dev/Shared/ChrImportException.cs
+++ dev/Shared/ChrImportException.cs
@@ -91,11 +91,13 @@ namespace FateExplorer.Shared
 
         // A constructor is needed for serialization when an
         // exception propagates from a remoting server to the client.
+        [Obsolete]
         protected ChrImportException(System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 
 
         // GetObjectData performs a custom serialization. 
+        [Obsolete]
         public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context)
         {
