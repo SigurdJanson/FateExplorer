7b66a8822d18644222694c12914327ccde194c40
Jan <59577271+SigurdJanson@users.noreply.github.com>
2025-10-20 00:37:21 +0200

    New class to represent a day of week
    A more flexible approach to the `System.DayOfWeek` enum

---
+++ Aventuria/Calendar/Weekday.cs
@@ -0,0 +1,115 @@
+﻿using System.Numerics;
+
+namespace Aventuria.Calendar;
+
+
+
+
+public readonly struct Weekday : IComparable,
+    IEquatable<Weekday>, IEqualityOperators<Weekday, Weekday, bool>, IEqualityOperators<Weekday, int, bool>
+{
+    public readonly int Day { get; }
+    public CalendarWeek Designation { get; init; }
+
+    public Weekday(int dayNumber, DesignationOfWeek designation)
+    {
+        Day = dayNumber;
+        Designation = GetCalendarWeek(designation);
+    }
+
+    public Weekday(int dayNumber, CalendarWeek designation)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(dayNumber, designation.Min);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(dayNumber, designation.Max);
+        Day = dayNumber;
+        Designation = designation;
+    }
+
+
+    public CalendarWeek GetCalendarWeek() => Designation;
+
+    public static CalendarWeek GetCalendarWeek(DesignationOfWeek designation) => designation switch
+    {
+        DesignationOfWeek.Bosparan => new BosparanWeek(),
+        DesignationOfWeek.Nameless => new NamelessWeek(),
+        DesignationOfWeek.Novadi => new NovadiWeek(),
+        DesignationOfWeek.Gjalsker => new GjalskerWeek(),
+        DesignationOfWeek.Lizardian => new LizardianWeek(),
+        _ => throw new NotImplementedException($"Calendar week for designation {designation} is not implemented.")
+    };
+
+    #region Implement IComparable
+    public readonly int CompareTo(object? other)
+        => other is not null ? Day.CompareTo(((Weekday)other).Day) : throw new ArgumentNullException(nameof(other));
+    #endregion
+
+
+    #region Implement IEquatable<Weekday>
+    public readonly bool Equals(Weekday other)
+        => Day == other.Day && Designation == other.Designation;
+    #endregion
+
+
+    #region Implement IEqualityOperators
+    static bool IEqualityOperators<Weekday, Weekday, bool>.operator ==(Weekday left, Weekday right)
+        => left.Equals(right);
+
+    static bool IEqualityOperators<Weekday, Weekday, bool>.operator !=(Weekday left, Weekday right)
+        => !left.Equals(right);
+
+
+    static bool IEqualityOperators<Weekday, int, bool>.operator ==(Weekday left, int right)
+        => left.Day == right;
+
+    static bool IEqualityOperators<Weekday, int, bool>.operator !=(Weekday left, int right)
+        => left.Day != right;
+    #endregion
+
+
+    #region Override Object Methods
+    public override readonly bool Equals(object? obj)
+        => obj is not null && Day == ((Weekday)obj).Day && Designation == ((Weekday)obj).Designation;
+
+    public override readonly int GetHashCode() => Designation.GetHashCode() + (Day.GetHashCode() << 16);
+
+    public override readonly string ToString() => $"{Day}";
+    #endregion
+
+
+    #region Comparison Operators
+    public static bool operator ==(Weekday left, Weekday right)
+        => left.Equals(right);
+
+    public static bool operator !=(Weekday left, Weekday right)
+        => !left.Equals(right);
+
+    public static bool operator <(Weekday left, Weekday right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator <=(Weekday left, Weekday right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >(Weekday left, Weekday right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    public static bool operator >=(Weekday left, Weekday right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
+    #endregion
+
+    #region Casting Operators
+    public static implicit operator int(Weekday d) => d.Day;
+    //public static explicit operator Weekday(byte b) => new(b, new BosparanWeek());
+    #endregion
+}
+
+
+
+
---
+++ RollLogicTests/Aventuria/Calendar/WeekdayTests.cs
@@ -0,0 +1,192 @@
+﻿using Aventuria.Calendar;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests.Aventuria.Calendar;
+
+[TestFixture]
+public class WeekdayTests
+{
+
+    [Test]
+    public void GetCalendarWeek()
+    {
+        // Arrange
+        var weekday = new Weekday(2, DesignationOfWeek.Novadi);
+        Type ExpectedType = typeof(NovadiWeek);
+        // Act
+        CalendarWeek result = weekday.GetCalendarWeek();
+
+        // Assert
+        Assert.That(result, Is.TypeOf(ExpectedType));
+    }
+
+
+
+    [Test]
+    [TestCase(DesignationOfWeek.Bosparan, typeof(BosparanWeek))]
+    [TestCase(DesignationOfWeek.Nameless, typeof(NamelessWeek))]
+    public void GetCalendarWeek_Static(DesignationOfWeek designation, Type expectedType)
+    {
+        // Arrange
+        // Act
+        var result = Weekday.GetCalendarWeek(designation);
+
+        // Assert
+        Assert.That(result, Is.TypeOf(expectedType));
+    }
+
+
+
+    [Test]
+    [TestCase(7, 7, DesignationOfWeek.Bosparan, ExpectedResult = 0, Category = "Culture is different")]
+    [TestCase(1, 1, DesignationOfWeek.Novadi, ExpectedResult = 0, Category = "Culture is different")]
+    [TestCase(1, 2, DesignationOfWeek.Novadi, ExpectedResult = -1, Category = "Day is different")]
+    [TestCase(2, 1, DesignationOfWeek.Bosparan, ExpectedResult = 1, Category = "Day is different")]
+    [TestCase(9, 9, DesignationOfWeek.Novadi, ExpectedResult = 0, Category = "Same")]
+    public int CompareTo_SameCulture(int day1, int day2, DesignationOfWeek cal1)
+    {
+        // Arrange
+        Weekday weekday = new (day1, cal1);
+        Weekday other = new(day2, cal1);
+
+        // Act
+        var result = weekday.CompareTo(other);
+
+        // Assert
+        return result;
+    }
+
+    //[Test]
+    //[TestCase(7, 7, DesignationOfWeek.Bosparan, ExpectedResult = 0, Category = "Culture is different")]
+    //[TestCase(1, 1, DesignationOfWeek.Novadi, ExpectedResult = 0, Category = "Culture is different")]
+    //[TestCase(1, 2, DesignationOfWeek.Novadi, ExpectedResult = 1, Category = "Day is different")]
+    //[TestCase(2, 1, DesignationOfWeek.Bosparan, ExpectedResult = -1, Category = "Day is different")]
+    //[TestCase(9, 9, DesignationOfWeek.Novadi, ExpectedResult = 0, Category = "Same")]
+    //public int CompareTo_DifferentCulture(int day1, int day2, DesignationOfWeek cal1, DesignationOfWeek cal2)
+    //{
+    //    // Arrange
+    //    Weekday weekday = new(day1, cal1);
+    //    Weekday other = new(day2, cal1);
+
+    //    // Act
+    //    var result = weekday.CompareTo(other);
+
+    //    // Assert
+    //    return result;
+    //}
+
+
+
+
+
+    [Test]
+    [TestCase(7, 7, DesignationOfWeek.Bosparan, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Culture is different")]
+    [TestCase(1, 1, DesignationOfWeek.Novadi, DesignationOfWeek.Bosparan, ExpectedResult = false, Category = "Culture is different")]
+    [TestCase(3, 7, DesignationOfWeek.Novadi, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Day is different")]
+    [TestCase(2, 1, DesignationOfWeek.Novadi, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Day is different")]
+    [TestCase(9, 9, DesignationOfWeek.Novadi, DesignationOfWeek.Novadi, ExpectedResult = true, Category = "Same")]
+    public bool Equatable_Equals(int day1, int day2, DesignationOfWeek cal1, DesignationOfWeek cal2)
+    {
+        // Arrange
+        Weekday weekday = new(day1, cal1);
+        Weekday other = new(day2, cal2);
+
+        // Act
+        var result = weekday.Equals(other);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase(7, DesignationOfWeek.Bosparan, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Different")]
+    [TestCase(1, DesignationOfWeek.Novadi, DesignationOfWeek.Bosparan, ExpectedResult = false, Category = "Different")]
+    [TestCase(3, DesignationOfWeek.Novadi, DesignationOfWeek.Novadi, ExpectedResult = true, Category = "Same")]
+    public bool Equals_SameDay(int day, DesignationOfWeek cal1, DesignationOfWeek cal2)
+    {
+        // Arrange
+        Weekday weekday = new(day, cal1);
+        Weekday other = new(day, cal2);
+
+        // Act
+        var result = weekday.Equals(other);
+
+        // Assert
+        return result;
+    }
+
+
+    [Test]
+    [TestCase(7, 6, DesignationOfWeek.Bosparan, ExpectedResult = false, Category = "Different")]
+    [TestCase(1, 2, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Different")]
+    [TestCase(3, 3, DesignationOfWeek.Novadi, ExpectedResult = true, Category = "Same")]
+    public bool Equals_SameDesignation(int day1, int day2, DesignationOfWeek cal)
+    {
+        // Arrange
+        Weekday weekday = new(day1, cal);
+        Weekday other = new(day2, cal);
+
+        // Act
+        var result = weekday.Equals(other);
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase(7, 6, DesignationOfWeek.Bosparan, ExpectedResult = false, Category = "Different")]
+    [TestCase(1, 2, DesignationOfWeek.Novadi, ExpectedResult = false, Category = "Different")]
+    [TestCase(3, 3, DesignationOfWeek.Novadi, ExpectedResult = true, Category = "Same")]
+    public bool EqualsAsObject_SameDesignation(int day1, int day2, DesignationOfWeek cal)
+    {
+        // Arrange
+        Weekday weekday = new(day1, cal);
+        object other = new Weekday(day2, cal);
+
+        // Act
+        var result = weekday.Equals(other);
+
+        // Assert
+        return result;
+    }
+
+
+
+
+
+    [Test]
+    [TestCase(7, DesignationOfWeek.Bosparan, ExpectedResult = 458759)]
+    [TestCase(1, DesignationOfWeek.Novadi, ExpectedResult = 65545)]
+    public int GetHashCode(int day, DesignationOfWeek calendar)
+    {
+        // Arrange
+        var weekday = new Weekday(day, calendar);
+
+        // Act
+        var result = weekday.GetHashCode();
+
+        // Assert
+        return result;
+    }
+
+
+
+    [Test]
+    [TestCase(7, DesignationOfWeek.Bosparan, ExpectedResult = "7")]
+    [TestCase(1, DesignationOfWeek.Novadi, ExpectedResult = "1")]
+    public string ToString(int day, DesignationOfWeek calendar)
+    {
+        // Arrange
+        Weekday weekday = new(day, calendar);
+
+        // Act
+        var result = weekday.ToString();
+
+        // Assert
+        return result;
+    }
+}
