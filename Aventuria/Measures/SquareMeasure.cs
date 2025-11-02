c0a237292a26eb15a900df4650ad7b29b2150b43
Jan <59577271+SigurdJanson@users.noreply.github.com>
2025-11-02 21:30:47 +0100

    New class for squared measures

---
+++ Aventuria/Measures/SquareMeasure.cs
@@ -0,0 +1,117 @@
+﻿using System.Numerics;
+
+namespace Aventuria.Measures;
+
+/// <summary>
+/// Represents a two-dimensional area measurement using the unit "anglepace" (i.e. square meters). Provides arithmetic, comparison,
+/// and formatting operations for square measures.
+/// </summary>
+/// <remarks>SquareMeasure supports standard arithmetic operations such as addition, subtraction, multiplication,
+/// and division, as well as equality and comparison checks. The struct is immutable and can be used in calculations
+/// involving area. Division by zero when using the division operators will result in a DivideByZeroException.
+/// SquareMeasure implements several numeric interfaces, allowing it to be used in generic numeric algorithms and
+/// collections.</remarks>
+public readonly struct SquareMeasure : IFormattable, // IParsable<TSelf>, ISpanParsable<TSelf>, 
+    IEquatable<SquareMeasure>, IEqualityOperators<SquareMeasure, SquareMeasure, bool>,
+    ISubtractionOperators<SquareMeasure, SquareMeasure, SquareMeasure>,
+    IDecrementOperators<SquareMeasure>,
+    IAdditionOperators<SquareMeasure, SquareMeasure, SquareMeasure>,
+    IIncrementOperators<SquareMeasure>,
+    IDivisionOperators<SquareMeasure, SquareMeasure, double>, IDivisionOperators<SquareMeasure, int, SquareMeasure>, IDivisionOperators<SquareMeasure, double, SquareMeasure>,
+    IMultiplyOperators<SquareMeasure, int, SquareMeasure>, IMultiplyOperators<SquareMeasure, double, SquareMeasure>,
+    // Todo: multiple an area with a 2 MeasureLength it gives an VolumeMeasure; Division of an Area by a Length gives a Length
+    IAdditiveIdentity<SquareMeasure, SquareMeasure>,
+    IMultiplicativeIdentity<SquareMeasure, SquareMeasure>,
+    IMinMaxValue<SquareMeasure>
+{
+    /// <summary>
+    /// The weight internally represented in Stone (i.e. kg in Earthen terms).
+    /// </summary>
+    private double Value { get; init; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="length">Sets the length in unit "anglepace"</param>
+    public SquareMeasure(double length)
+    {
+        Value = length;
+    }
+
+
+    /// <summary>
+    /// Returns the value as double in paces squared, i.e. anglepace.
+    /// </summary>
+    /// <param name="value">A <see cref="SquareMeasure"/> object</param>
+    public static explicit operator double(SquareMeasure value) => value.Value;
+
+
+
+    public override bool Equals(object? obj) // inherited from object
+        => obj is not null && Equals((SquareMeasure)obj);
+
+    public override int GetHashCode() // inherited from object
+    => Value.GetHashCode();
+
+
+
+    public override string? ToString() => Value.ToString();
+
+    public string ToString(string? format, IFormatProvider? formatProvider) // IFormattable // TODO: implement LengthMeasure::toString()
+    {
+        return Value.ToString(format, formatProvider); // TODO: Implement using DereCultureInfo
+    }
+
+
+    public bool Equals(SquareMeasure other) // IEquatable
+        => Value == other.Value;
+
+    public static bool operator ==(SquareMeasure left, SquareMeasure right) // IEqualityOperators
+        => left.Equals(right);
+
+    public static bool operator !=(SquareMeasure left, SquareMeasure right) // IEqualityOperators
+        => !left.Equals(right);
+
+    public static SquareMeasure operator -(SquareMeasure left, SquareMeasure right) // ISubtractionOperators
+        => new(left.Value - right.Value);
+
+    public static SquareMeasure operator --(SquareMeasure value) // IDecrementOperators
+        => new(value.Value - 1);
+
+    public static SquareMeasure operator +(SquareMeasure left, SquareMeasure right) // IAdditionOperators
+        => new(left.Value + right.Value);
+
+    public static SquareMeasure operator ++(SquareMeasure value) // IIncrementOperators
+        => new(value.Value + 1);
+
+    public static double operator /(SquareMeasure left, SquareMeasure right) // IDivisionOperators
+        => left.Value / right.Value;
+
+    public static SquareMeasure operator /(SquareMeasure left, int right) // IDivisionOperators
+    {
+        if (right == 0)
+            throw new DivideByZeroException("Division by zero.");
+        return new(left.Value / right);
+    }   
+
+    public static SquareMeasure operator /(SquareMeasure left, double right) // IDivisionOperators
+    {
+        if (right == 0)
+            throw new DivideByZeroException("Division by zero.");
+        return new(left.Value / right);
+    }
+
+    public static SquareMeasure operator *(SquareMeasure left, int right) // IMultiplyOperators
+        => new(left.Value * right);
+
+    public static SquareMeasure operator *(SquareMeasure left, double right) // IMultiplyOperators
+        => new(left.Value * right);
+
+    public static SquareMeasure AdditiveIdentity => new(0); // IAdditiveIdentity
+
+    public static SquareMeasure MultiplicativeIdentity => new(1); // IMultiplicativeIdentity
+
+    public static SquareMeasure MinValue => new(double.MinValue); // IMinMaxValue
+
+    public static SquareMeasure MaxValue => new(double.MaxValue); // IMinMaxValue
+}
---
+++ RollLogicTests/Measures/SquareMeasureTests.cs
@@ -0,0 +1,405 @@
+﻿using Aventuria.Measures;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Globalization;
+
+namespace UnitTests.Measures;
+
+[TestFixture]
+public class SquareMeasureTests
+{
+    private MockRepository mockRepository;
+
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+    }
+
+    private SquareMeasure CreateSquareMeasure(double value)
+    {
+        return new SquareMeasure(value);
+    }
+
+
+    [Test]
+    [TestCase(1.23)]
+    public void ObjEquals_Same_ReturnsTrue(double value)
+    {
+#nullable enable
+        // Arrange
+        var squareMeasure = CreateSquareMeasure(value);
+        object? obj = CreateSquareMeasure(value);
+
+        // Act
+        var result = squareMeasure.Equals(obj);
+
+        // Assert
+        Assert.That(result, Is.True);
+#nullable restore
+    }
+
+    [Test]
+    [TestCase(1.23, 1.230000001)]
+    public void ObjEquals_Different_ReturnsFalse(double value, double other)
+    {
+#nullable enable
+        // Arrange
+        var squareMeasure = CreateSquareMeasure(value);
+        object? obj = CreateSquareMeasure(other);
+
+        // Act
+        var result = squareMeasure.Equals(obj);
+
+        // Assert
+        Assert.That(result, Is.False);
+#nullable restore
+    }
+
+    [Test]
+    [TestCase(1.23)]
+    public void ObjEquals_Null_ReturnsFalse(double value)
+    {
+#nullable enable
+        // Arrange
+        var squareMeasure = CreateSquareMeasure(value);
+        object? obj = null;
+
+        // Act
+        var result = squareMeasure.Equals(obj);
+
+        // Assert
+        Assert.That(result, Is.False);
+#nullable restore
+    }
+
+
+
+    [Test]
+    [TestCase(double.MinValue)]
+    [TestCase(1.23)]
+    public void GetHashCode_StateUnderTest_ExpectedBehavior(double value)
+    {
+        // Arrange
+        var squareMeasure = this.CreateSquareMeasure(value);
+
+        // Act
+        var result = squareMeasure.GetHashCode();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(value.GetHashCode()));
+    }
+
+
+
+
+
+    #region Constructor Tests
+
+    [Test]
+    public void Constructor_ShouldInitializeValueCorrectly()
+    {
+        // Arrange
+        double length = 10.5;
+
+        // Act
+        var squareMeasure = new SquareMeasure(length);
+
+        // Assert
+        Assert.That((double)squareMeasure, Is.EqualTo(length));
+    }
+
+
+    [Test]
+    public void Constructor_WithDoubleMaxValue_ShouldInitializeCorrectly()
+    {
+        // Arrange & Act
+        var squareMeasure = new SquareMeasure(double.MaxValue);
+
+        // Assert
+        Assert.That((double)squareMeasure, Is.EqualTo(double.MaxValue));
+    }
+
+    [Test]
+    public void Constructor_WithDoubleMinValue_ShouldInitializeCorrectly()
+    {
+        // Arrange & Act
+        var squareMeasure = new SquareMeasure(double.MinValue);
+
+        // Assert
+        Assert.That((double)squareMeasure, Is.EqualTo(double.MinValue));
+    }
+
+
+
+    #endregion
+
+
+
+    #region Equality Tests
+
+    [TestCase(10.0, 10.0, true)]
+    [TestCase(10.0, 20.0, false)]
+    public void Equals_ShouldReturnTrueForSameValues(double left, double right, bool expected)
+    {
+        // Arrange
+        var squareMeasure1 = new SquareMeasure(left);
+        var squareMeasure2 = new SquareMeasure(right);
+
+        // Act
+        bool result = squareMeasure1.Equals(squareMeasure2);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 10.0, true)]
+    [TestCase(10.0, 10.0000000000001, false)]
+    [TestCase(10.0, 20.0, false)]
+    public void EqualityOperator_ShouldReturnCorrectResult(double left, double right, bool expected)
+    {
+        // Arrange
+        var squareMeasure1 = new SquareMeasure(left);
+        var squareMeasure2 = new SquareMeasure(right);
+
+        // Act
+        bool result = squareMeasure1 == squareMeasure2;
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 10.0, false)]
+    [TestCase(10.0, 20.0, true)]
+    public void InequalityOperator_ShouldReturnCorrectResult(double left, double right, bool expected)
+    {
+        // Arrange
+        var squareMeasure1 = new SquareMeasure(left);
+        var squareMeasure2 = new SquareMeasure(right);
+
+        // Act
+        bool result = squareMeasure1 != squareMeasure2;
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    #endregion
+
+
+
+    #region Arithmetic Operators Tests
+
+    [TestCase(10.0, 5.0, 15.0)]
+    public void AdditionOperator_ShouldReturnCorrectResult(double left, double right, double expected)
+    {
+        // Arrange
+        var squareMeasure1 = new SquareMeasure(left);
+        var squareMeasure2 = new SquareMeasure(right);
+
+        // Act
+        var result = squareMeasure1 + squareMeasure2;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 7.0, 3.0)]
+    public void SubtractionOperator_ShouldReturnCorrectResult(double left, double right, double expected)
+    {
+        // Arrange
+        var squareMeasure1 = new SquareMeasure(left);
+        var squareMeasure2 = new SquareMeasure(right);
+
+        // Act
+        var result = squareMeasure1 - squareMeasure2;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 2, 5.0)]
+    public void DivisionByIntOperator_ShouldReturnCorrectResult(double left, int right, double expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(left);
+
+        // Act
+        var result = squareMeasure / right;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 2.0, 5.0)]
+    public void DivisionByDoubleOperator_ShouldReturnCorrectResult(double left, double right, double expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(left);
+
+        // Act
+        var result = squareMeasure / right;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 2, 20.0)]
+    public void MultiplicationByIntOperator_ShouldReturnCorrectResult(double left, int right, double expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(left);
+
+        // Act
+        var result = squareMeasure * right;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.0, 2.0, 20.0)]
+    public void MultiplicationByDoubleOperator_ShouldReturnCorrectResult(double left, double right, double expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(left);
+
+        // Act
+        var result = squareMeasure * right;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void IncrementOperator_ShouldReturnCorrectResult()
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(10.0);
+
+        // Act
+        var result = squareMeasure++;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(10.0));
+        Assert.That((double)squareMeasure, Is.EqualTo(11.0));
+    }
+
+    [Test]
+    public void DecrementOperator_ShouldReturnCorrectResult()
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(10.0);
+
+        // Act
+        var result = squareMeasure--;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(10.0));
+        Assert.That((double)squareMeasure, Is.EqualTo(9.0));
+    }
+
+    #endregion
+
+
+
+    #region Identity and Min/Max Value Tests
+
+    [Test]
+    public void AdditiveIdentity_ShouldReturnZero()
+    {
+        // Arrange & Act
+        var result = SquareMeasure.AdditiveIdentity;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(0.0));
+    }
+
+    [Test]
+    public void MultiplicativeIdentity_ShouldReturnOne()
+    {
+        // Arrange & Act
+        var result = SquareMeasure.MultiplicativeIdentity;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(1.0));
+    }
+
+    [Test]
+    public void MinValue_ShouldReturnDoubleMinValue()
+    {
+        // Arrange & Act
+        var result = SquareMeasure.MinValue;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(double.MinValue));
+    }
+
+    [Test]
+    public void MaxValue_ShouldReturnDoubleMaxValue()
+    {
+        // Arrange & Act
+        var result = SquareMeasure.MaxValue;
+
+        // Assert
+        Assert.That((double)result, Is.EqualTo(double.MaxValue));
+    }
+
+    #endregion
+
+
+
+    #region ToString Tests
+
+    [TestCase(10.0, "10")]
+    [TestCase(10.5, "10.5")]
+    [SetCulture("en-US")]
+    public void ToString_ShouldReturnCorrectStringRepresentation(double value, string expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(value);
+
+        // Act
+        string result = squareMeasure.ToString();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase(10.5, "F2", "en-US", "10.50")]
+    [TestCase(10.5, "F1", "en-US", "10.5")]
+    public void ToStringWithFormat_ShouldReturnCorrectStringRepresentation(double value, string format, string cultureName, string expected)
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(value);
+        var cultureInfo = new CultureInfo(cultureName);
+
+        // Act
+        string result = squareMeasure.ToString(format, cultureInfo);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    #endregion
+
+
+    #region Edge case
+
+    [Test]
+    public void Division_Int_DivByZero_ShouldThrowDivideByZeroException()
+    {
+        // Arrange
+        var squareMeasure = new SquareMeasure(10.0);
+
+        // Act & Assert
+        Assert.That(() => _ = squareMeasure / 0.0, Throws.TypeOf<DivideByZeroException>());
+    }
+
+    #endregion
+}
+
