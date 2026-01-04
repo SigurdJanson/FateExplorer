c9a80ec55660f135fbd711e178eab5780d169d63
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-01-04 17:27:55 +0100

    Added dependency on root value to dodge value

--- RollLogicTests/CharacterModel/DodgeMTests.cs
+++ RollLogicTests/CharacterModel/DodgeMTests.cs
@@ -1,91 +1,236 @@
 ﻿using FateExplorer.CharacterModel;
-using FateExplorer.Shared;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 
-namespace UnitTests.CharacterModel
+namespace UnitTests.CharacterModel;
+
+
+
+[TestFixture]
+public class DodgeMTests
 {
-    [TestFixture]
-    public class DodgeMTests
+    private MockRepository mockRepository;
+
+    private Mock<ICharacterM> mockCharacterM;
+
+    [SetUp]
+    public void SetUp()
     {
-        private MockRepository mockRepository;
+        this.mockRepository = new MockRepository(MockBehavior.Strict);
 
-        private Mock<ICharacterM> mockCharacterM;
-
-        [SetUp]
-        public void SetUp()
-        {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
-
-            this.mockCharacterM = this.mockRepository.Create<ICharacterM>();
-        }
-
-        private DodgeM CreateDodgeM()
-        {
-            return new DodgeM(this.mockCharacterM.Object);
-        }
-
-
-        [Test, Sequential]
-        public void Dodge([Values(4,8,16,19)] int doVal, [Values(2,4,8,10)] int Expected)
-        {
-            // Arrange
-            mockCharacterM.Setup(c => c.GetAbility(It.Is<string>(s => s == AbilityM.AGI)))
-                .Returns(doVal);
-
-            // Act
-            var dodgeM = this.CreateDodgeM();
-
-            // Assert
-            Assert.That(Expected, Is.EqualTo(dodgeM.Effective));
-            mockCharacterM.Verify(c => c.GetAbility(It.Is<string>(s => s == AbilityM.AGI)), Times.Once);
-        }
-
-        [Test]
-        public void ComputeDodge_Layariel()
-        {
-            // Arrange
-            int EffectiveAgility = HeroWipfelglanz.AbilityValues[AbilityM.AGI];
-
-            // Act
-            var result = DodgeM.ComputeDodge(EffectiveAgility);
-
-            // Assert
-            Assert.That(HeroWipfelglanz.Dodge, Is.EqualTo(result));
-            this.mockRepository.VerifyAll();
-        }
-
-
-
-        [Test]
-        public void ComputeDodge_Arbosch()
-        {
-            // Arrange
-            int EffectiveAgility = HeroArbosch.AbilityValues[AbilityM.AGI];
-
-            // Act
-            var result = DodgeM.ComputeDodge(EffectiveAgility);
-
-            // Assert
-            Assert.That(HeroArbosch.Dodge, Is.EqualTo(result));
-            this.mockRepository.VerifyAll();
-        }
-
-
-        [Test]
-        public void ComputeDodge_Grassberger()
-        {
-            // Arrange
-            int EffectiveAgility = HeroGrassberger.AbilityValues[AbilityM.AGI];
-
-            // Act
-            var result = DodgeM.ComputeDodge(EffectiveAgility);
-
-            // Assert
-            Assert.That(HeroGrassberger.Dodge, Is.EqualTo(result));
-            this.mockRepository.VerifyAll();
-        }
+        this.mockCharacterM = this.mockRepository.Create<ICharacterM>();
     }
+
+    #region Test Helpers
+
+    private DodgeM CreateDodgeM()
+    {
+        return new DodgeM(this.mockCharacterM.Object);
+    }
+
+
+    private static Mock<ICharacterM> CreateHeroMock(int agility)
+    {
+        var heroMock = new Mock<ICharacterM>();
+        heroMock
+            .Setup(h => h.GetAbility(AbilityM.AGI))
+            .Returns(agility);
+
+        return heroMock;
+    }
+
+    #endregion Test Helpers
+
+
+
+    #region Construction
+
+    [Test]
+    public void Constructor_InitializesMinAndMax()
+    {
+        // Arrange
+        var hero = CreateHeroMock(10);
+
+        // Act
+        var dodge = new DodgeM(hero.Object);
+
+        // Assert
+        Assert.That(dodge.Min, Is.EqualTo(0));
+        Assert.That(dodge.Max, Is.EqualTo(20));
+    }
+
+
+    [Test]
+    public void Constructor_SetsDependencyToAgility()
+    {
+        // Arrange
+        var hero = CreateHeroMock(10);
+
+        // Act
+        var dodge = new DodgeM(hero.Object);
+        var deps = dodge.GetDependencies();
+
+        // Assert
+        Assert.That(deps, Is.EquivalentTo(new[] { AbilityM.AGI }));
+    }
+
+    #endregion Construction
+
+
+
+
+    #region Dependency Handling
+
+    [Test]
+    public void DependsOn_Agility_ReturnsTrue()
+    {
+        // Arrange
+        var hero = CreateHeroMock(10);
+        var dodge = new DodgeM(hero.Object);
+
+        // Act
+        var result = dodge.DependsOn(AbilityM.AGI);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void DependsOn_OtherAttribute_ReturnsFalse()
+    {
+        // Arrange
+        var hero = CreateHeroMock(10);
+        var dodge = new DodgeM(hero.Object);
+
+        // Act
+        var result = dodge.DependsOn("OTHER");
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    #endregion Dependency Handling
+
+
+
+
+    #region Dependency Change Propagation
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(9)]
+    [TestCase(20)]
+    public void DependencyHasChanged_UpdatesEffectiveValue(int newAgility)
+    {
+        // Arrange
+        var hero = CreateHeroMock(0);
+        var dodge = new DodgeM(hero.Object);
+
+        // Act
+        dodge.DependencyHasChanged(AbilityM.AGI, newAgility);
+
+        // Assert
+        Assert.That(dodge.Effective, Is.EqualTo(DodgeM.ComputeDodge(newAgility)));
+    }
+
+    #endregion Dependency Change Propagation
+
+
+
+
+    #region Range Enforcement
+
+    [Test]
+    public void DependencyChange_ResultAboveMax_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var hero = CreateHeroMock(0);
+        var dodge = new DodgeM(hero.Object);
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => dodge.DependencyHasChanged(AbilityM.AGI, 50));
+    }
+
+    [Test]
+    public void DependencyChange_ResultBelowMin_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var hero = CreateHeroMock(0);
+        var dodge = new DodgeM(hero.Object);
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => dodge.DependencyHasChanged(AbilityM.AGI, -1));
+    }
+
+    #endregion
+
+
+
+    #region Static ComputeDodge Method
+
+    [Test, Sequential]
+    public void Dodge([Values(4,8,16,19)] int doVal, [Values(2,4,8,10)] int Expected)
+    {
+        // Arrange
+        mockCharacterM.Setup(c => c.GetAbility(It.Is<string>(s => s == AbilityM.AGI)))
+            .Returns(doVal);
+
+        // Act
+        var dodgeM = this.CreateDodgeM();
+
+        // Assert
+        Assert.That(Expected, Is.EqualTo(dodgeM.Effective));
+        mockCharacterM.Verify(c => c.GetAbility(It.Is<string>(s => s == AbilityM.AGI)), Times.Once);
+    }
+
+
+    [Test]
+    public void ComputeDodge_Layariel()
+    {
+        // Arrange
+        int EffectiveAgility = HeroWipfelglanz.AbilityValues[AbilityM.AGI];
+
+        // Act
+        var result = DodgeM.ComputeDodge(EffectiveAgility);
+
+        // Assert
+        Assert.That(HeroWipfelglanz.Dodge, Is.EqualTo(result));
+        this.mockRepository.VerifyAll();
+    }
+
+
+
+    [Test]
+    public void ComputeDodge_Arbosch()
+    {
+        // Arrange
+        int EffectiveAgility = HeroArbosch.AbilityValues[AbilityM.AGI];
+
+        // Act
+        var result = DodgeM.ComputeDodge(EffectiveAgility);
+
+        // Assert
+        Assert.That(HeroArbosch.Dodge, Is.EqualTo(result));
+        this.mockRepository.VerifyAll();
+    }
+
+
+    [Test]
+    public void ComputeDodge_Grassberger()
+    {
+        // Arrange
+        int EffectiveAgility = HeroGrassberger.AbilityValues[AbilityM.AGI];
+
+        // Act
+        var result = DodgeM.ComputeDodge(EffectiveAgility);
+
+        // Assert
+        Assert.That(HeroGrassberger.Dodge, Is.EqualTo(result));
+        this.mockRepository.VerifyAll();
+    }
+
+    #endregion Static ComputeDodge Method
 }
--- dev/CharacterModel/DodgeM.cs
+++ dev/CharacterModel/DodgeM.cs
@@ -1,38 +1,37 @@
-﻿using FateExplorer.Shared;
+﻿namespace FateExplorer.CharacterModel;
 
-namespace FateExplorer.CharacterModel
+/// <summary>
+/// Represents the competence to dodge
+/// </summary>
+public class DodgeM : DerivedValue
 {
+    //--private readonly ICharacterM Hero;
+
     /// <summary>
-    /// Represents the competence to dodge
+    /// Constructor
     /// </summary>
-    public class DodgeM : DerivedValue
+    /// <param name="hero">The character of this dodge competence</param>
+    public DodgeM(ICharacterM hero) : base(ComputeDodge(hero.GetAbility(AbilityM.AGI)))
     {
-        //--private readonly ICharacterM Hero;
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="hero">The character of this dodge competence</param>
-        public DodgeM(ICharacterM hero) : base(ComputeDodge(hero.GetAbility(AbilityM.AGI)))
-        {
-            //--Hero = hero;
-            Min = 0;
-            Max = 20;
-            DependencyId = new string[1] { AbilityM.AGI };
-        }
-
-        /// <summary>
-        /// Computes a valid dodge value from a given effect
-        /// </summary>
-        /// <param name="EffectiveAgility">The agility of the character</param>
-        /// <returns>An effective dodge value</returns>
-        public static int ComputeDodge(int EffectiveAgility)
-            => EffectiveAgility / 2 + EffectiveAgility % 2;
-
-
-        ///// <summary>
-        ///// The imported dodge value
-        ///// </summary>
-        //public int Value { get; protected set; }
+        //--Hero = hero;
+        Min = 0;
+        Max = 20;
+        DependencyId = [AbilityM.AGI];
     }
+
+    /// <summary>
+    /// Computes a valid dodge value from a given effect
+    /// </summary>
+    /// <param name="EffectiveAgility">The agility of the character</param>
+    /// <returns>An effective dodge value</returns>
+    public static int ComputeDodge(int EffectiveAgility)
+        => EffectiveAgility / 2 + EffectiveAgility % 2;
+
+    
+    /// <inheritdoc/>
+    protected override void UpdateOnDependencyChange(string attrId, int newValue)
+    {
+        Effective = ComputeDodge(newValue);
+    }
+
 }
