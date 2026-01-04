b85c542cab643faea2d17f80af57bea71e5aa954
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-01-04 17:06:03 +0100

    Improved tests for character attributes
    * Added some tests for derived values
    * Created tests for root values

---
+++ RollLogicTests/CharacterModel/DerivedValueTests.cs
@@ -0,0 +1,277 @@
+﻿using FateExplorer.CharacterModel;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests.CharacterModel;
+
+[TestFixture()]
+public class DerivedValueTests
+{
+    const int DefaultMinValue = -100, DefaultMaxValue = 100;
+
+
+    #region Test Helpers
+
+    private sealed class TestDerivedValue : DerivedValue
+    {
+        public const int MinValue = 2, MaxValue = 18;
+
+        public string LastAttrId { get; private set; }
+        public int LastNewValue { get; private set; }
+        public int CallCount { get; private set; }
+
+        public TestDerivedValue(int value, string[] dependencies)
+            : base(value)
+        {
+            DependencyId = dependencies;
+            Min = MinValue;
+            Max = MaxValue;
+        }
+
+        protected override void UpdateOnDependencyChange(string attrId, int newValue)
+        {
+            CallCount++;
+            LastAttrId = attrId;
+            LastNewValue = newValue;
+        }
+    }
+
+    private static DerivedValue CreateDerivedValue(int value)
+    {
+        return new DerivedValue(value)
+        {
+            Id = "attr.derived"
+        };
+    }
+
+    #endregion Test Helpers
+
+
+
+    #region Construction
+
+    [Test]
+    public void Init_Test([Values(1, 2, 5, 10)] int value)
+    {
+        // Arrange
+        var derivedValue = new DerivedValue(value);
+
+        // Act
+
+        // Assert
+        Assert.That(derivedValue.Imported, Is.EqualTo(value));
+        Assert.That(derivedValue.True, Is.EqualTo(value));
+        Assert.That(derivedValue.Effective, Is.EqualTo(value));
+        Assert.That((int)derivedValue, Is.EqualTo(value));
+    }
+
+
+
+    [Test]
+    public void Constructor_InitializesMinAndMaxDefaults()
+    {
+        // Arrange
+        var derived = CreateDerivedValue(0);
+
+        // Act & Assert
+        Assert.That(derived.Min, Is.EqualTo(DefaultMinValue));
+        Assert.That(derived.Max, Is.EqualTo(DefaultMaxValue));
+    }
+
+
+    [Test]
+    public void Constructor_InitializesEmptyDependencies()
+    {
+        // Arrange
+        var derived = CreateDerivedValue(5);
+
+        // Act
+        var deps = derived.GetDependencies();
+
+        // Assert
+        Assert.That(deps, Is.Not.Null);
+        Assert.That(deps, Is.Empty);
+    }
+
+
+    #endregion Construction
+
+
+
+    #region Dependency Inspection
+
+    [Test]
+    public void GetDependencies_ReturnsSameInstance()
+    {
+        // Arrange
+        var deps = new[] { "attr.a", "attr.b" };
+        var derived = new TestDerivedValue(0, deps);
+
+        // Act
+        var result = derived.GetDependencies();
+
+        // Assert
+        Assert.That(result, Is.SameAs(deps));
+    }
+
+
+    [TestCase("attr.a", true)]
+    [TestCase("attr.b", true)]
+    [TestCase("attr.c", false)]
+    public void DependsOn_ReturnsExpectedResult(string dependency, bool expected)
+    {
+        // Arrange
+        var derived = new TestDerivedValue(0, ["attr.a", "attr.b"]);
+
+        // Act
+        var result = derived.DependsOn(dependency);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void DependsOn_NoDependencies_ReturnsFalse()
+    {
+        // Arrange
+        var derived = new TestDerivedValue(0, []);
+
+        // Act
+        var result = derived.DependsOn("attr.any");
+
+        // Assert
+        Assert.That(result, Is.False);
+    }
+
+    #endregion Dependency Inspection
+
+
+
+
+    #region Dependency Change Handling
+
+    [Test]
+    public void DependencyHasChanged_InvokesUpdateOnDependencyChange()
+    {
+        // Arrange
+        var derived = new TestDerivedValue(0, new[] { "attr.a" });
+
+        // Act
+        derived.DependencyHasChanged("attr.a", 42);
+
+        // Assert
+        Assert.That(derived.CallCount, Is.EqualTo(1));
+        Assert.That(derived.LastAttrId, Is.EqualTo("attr.a"));
+        Assert.That(derived.LastNewValue, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void DependencyHasChanged_MultipleCalls_AreAllForwarded()
+    {
+        // Arrange
+        var derived = new TestDerivedValue(0, ["attr.a"]);
+
+        // Act
+        derived.DependencyHasChanged("attr.a", 1);
+        derived.DependencyHasChanged("attr.a", 2);
+
+        // Assert
+        Assert.That(derived.CallCount, Is.EqualTo(2));
+        Assert.That(derived.LastNewValue, Is.EqualTo(2));
+    }
+
+    #endregion Dependency Change Handling
+
+
+
+    #region Range Enforcement
+
+    [Test]
+    public void True_SetOutsideDefaultRange_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var derived = CreateDerivedValue(5);
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => derived.True = DefaultMaxValue + 1);
+        Assert.Throws<ArgumentOutOfRangeException>(() => derived.True = DefaultMinValue - 1);
+    }
+
+    [Test]
+    public void Effective_SetOutsideDefaultRange_ThrowsArgumentOutOfRangeException()
+    {
+        // Arrange
+        var derived = CreateDerivedValue(5);
+
+        // Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(() => derived.Effective = DefaultMaxValue + 1);
+        Assert.Throws<ArgumentOutOfRangeException>(() => derived.Effective = DefaultMinValue - 1);
+    }
+
+    #endregion
+
+
+
+    #region Value Setting
+    /// <summary>
+    /// Test for modifiers added by activatables.
+    /// </summary>
+    [Test]
+    public void AddOnSetup_Okay([Values(5, 10)] int Imported, [Values(-3, +3)] int Mod)
+    {
+        // Arrange
+        var derivedValue = new DerivedValue(Imported);
+
+        // Act
+        derivedValue.AddOnSetup(Mod);
+
+        // Assert
+        Assert.That(derivedValue.True, Is.EqualTo(Imported + Mod));
+        Assert.That(derivedValue.Effective, Is.EqualTo(Imported + Mod));
+    }
+
+
+
+
+    /// <summary>
+    /// Test for true value. Happy case.
+    /// </summary>
+    [Test]
+    public void True_Okay([Values(5, 10)] int Imported, 
+        [Values(-3, +3)] int ActivatableMod, [Values(5, 10, 15)] int True)
+    {
+        // Arrange
+        var derivedValue = new DerivedValue(Imported);
+
+        // Act
+        derivedValue.AddOnSetup(ActivatableMod);
+        derivedValue.True = True;
+
+        // Assert
+        Assert.That(derivedValue.True, Is.EqualTo(True));
+        Assert.That(derivedValue.Effective, Is.EqualTo(True));
+    }
+
+
+    /// <summary>
+    /// Test for effective value. Happy case.
+    /// </summary>
+    [Test]
+    public void Effective_Okay([Values(5, 10)] int Imported,
+        [Values(-3, +3)] int ActivatableMod, [Values(5, 10, 15)] int True,
+        [Values(5, 10, 15)] int Effective)
+    {
+        // Arrange
+        var derivedValue = new DerivedValue(Imported);
+
+        // Act
+        derivedValue.AddOnSetup(ActivatableMod);
+        derivedValue.True = True;
+        derivedValue.Effective = Effective;
+
+        // Assert
+        Assert.That(derivedValue.True, Is.EqualTo(True));
+        Assert.That(derivedValue.Effective, Is.EqualTo(Effective));
+    }
+    #endregion Value Setting
+}
---
+++ RollLogicTests/CharacterModel/RootValueTests.cs
@@ -0,0 +1,272 @@
+﻿using FateExplorer.CharacterModel;
+using NUnit.Framework;
+
+namespace UnitTests.CharacterModel;
+
+
+[TestFixture]
+public class RootValueTests
+{
+    #region Test Helpers
+
+    private static RootValue CreateRootValue(int imported, string id = "attr.test")
+    {
+        return new RootValue(imported)
+        {
+            Id = id
+        };
+    }
+
+    #endregion
+
+
+
+    #region Construction
+
+    [Test]
+    public void Constructor_InitializesImportedValue()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+
+        // Act
+        var value = root.Imported;
+
+        // Assert
+        Assert.That(value, Is.EqualTo(10));
+    }
+
+    [Test]
+    public void Constructor_InitializesTrueAndEffectiveEqualToImported()
+    {
+        // Arrange
+        var root = CreateRootValue(7);
+
+        // Act & Assert
+        Assert.That(root.True, Is.EqualTo(7));
+        Assert.That(root.Effective, Is.EqualTo(7));
+    }
+
+    #endregion
+
+
+
+
+    #region Imported Property
+    [TestCase(5, 8)]
+    [TestCase(0, -3)]
+    public void Imported_SetWithinRange_UpdatesTrueValue(int imported, int newImported)
+    {
+        // Arrange
+        var root = CreateRootValue(imported);
+
+        // Act
+        root.Imported = newImported;
+
+        // Assert
+        Assert.That(root.Imported, Is.EqualTo(newImported));
+        Assert.That(root.True, Is.EqualTo(newImported)); // True value should change, too
+        Assert.That(root.Effective, Is.EqualTo(newImported)); // effective value should change, too
+    }
+    #endregion
+
+
+
+
+    #region True Property
+
+    [TestCase(5, 8)]
+    [TestCase(0, -3)]
+    public void True_SetWithinRange_UpdatesTrueValue(int imported, int newTrue)
+    {
+        // Arrange
+        var root = CreateRootValue(imported);
+
+        // Act
+        root.True = newTrue;
+
+        // Assert
+        Assume.That(root.Imported, Is.EqualTo(imported)); // Just to be sure
+        Assert.That(root.True, Is.EqualTo(newTrue));
+    }
+
+
+
+    [Test]
+    public void True_ChangeTriggersStateChangedEvent()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        string receivedId = null;
+        int receivedValue = 0;
+
+        root.OnStateChanged += (id, value) =>
+        {
+            receivedId = id;
+            receivedValue = value;
+        };
+
+        // Act
+        root.True = 12;
+
+        // Assert
+        Assert.That(receivedId, Is.EqualTo(root.Id));
+        Assert.That(receivedValue, Is.EqualTo(root.Effective));
+    }
+
+    [Test]
+    public void True_SetToSameEffectiveValue_DoesNotTriggerStateChangedEvent()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        var callCount = 0;
+
+        root.OnStateChanged += (_, _) => callCount++;
+
+        // Act
+        root.True = 10;
+
+        // Assert
+        Assert.That(callCount, Is.EqualTo(0));
+    }
+
+    #endregion
+
+
+
+
+    #region Effective Property
+
+    [TestCase(10, 15)]
+    [TestCase(5, 2)]
+    public void Effective_SetWithinRange_UpdatesEffectiveValue(int imported, int newEffective)
+    {
+        // Arrange
+        var root = CreateRootValue(imported);
+
+        // Act
+        root.Effective = newEffective;
+
+        // Assert
+        Assert.That(root.Effective, Is.EqualTo(newEffective));
+    }
+
+
+    [Test]
+    public void Effective_ChangeTriggersStateChangedEvent()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        string receivedId = null;
+        int receivedValue = 0;
+
+        root.OnStateChanged += (id, value) =>
+        {
+            receivedId = id;
+            receivedValue = value;
+        };
+
+        // Act
+        root.Effective = 14;
+
+        // Assert
+        Assert.That(receivedId, Is.EqualTo(root.Id));
+        Assert.That(receivedValue, Is.EqualTo(14));
+    }
+
+    [Test]
+    public void Effective_SetToSameValue_DoesNotTriggerStateChangedEvent()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        var callCount = 0;
+
+        root.OnStateChanged += (_, _) => callCount++;
+
+        // Act
+        root.Effective = 10;
+
+        // Assert
+        Assert.That(callCount, Is.EqualTo(0));
+    }
+
+    #endregion
+
+
+
+
+    #region Interaction Between Imported, True and Effective
+
+    [Test]
+    public void True_ChangeAffectsEffectiveAndTriggersEventOnce()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        var callCount = 0;
+
+        root.OnStateChanged += (_, _) => callCount++;
+
+        // Act
+        root.True = 13;
+
+        // Assert
+        Assert.That(root.Effective, Is.EqualTo(13));
+        Assert.That(callCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Effective_ChangeDoesNotAffectTrue()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+
+        // Act
+        root.Effective = 15;
+
+        // Assert
+        Assert.That(root.True, Is.EqualTo(10));
+        Assert.That(root.Effective, Is.EqualTo(15));
+    }
+
+
+    [Test]
+    public void Effective_ChangesWithImported()
+    {
+        const int Value = 10, TrueDelta = 1, EffDelta = 2;
+        const int newImported = 15;
+        // Arrange
+        var root = CreateRootValue(Value);
+        root.True = Value + 1;
+        root.Effective = Value + TrueDelta + EffDelta;
+
+        // Act
+        root.Imported = newImported;
+
+        // Assert
+        Assert.That(root.True, Is.EqualTo(newImported + TrueDelta));
+        Assert.That(root.Effective, Is.EqualTo(newImported + TrueDelta + EffDelta));
+    }
+    #endregion
+
+
+
+
+    #region Implicit Int Conversion
+
+    [Test]
+    public void ImplicitConversion_ReturnsEffectiveValue()
+    {
+        // Arrange
+        var root = CreateRootValue(10);
+        root.Effective = 18;
+
+        // Act
+        int value = root;
+
+        // Assert
+        Assert.That(value, Is.EqualTo(18));
+    }
+
+    #endregion
+}
+
