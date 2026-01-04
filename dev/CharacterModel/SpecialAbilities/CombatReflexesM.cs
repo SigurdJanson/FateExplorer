cf76604097b56f538ad046cc22dc4257bfee247b
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-01-04 23:59:25 +0100

    Recognize Combat Reflexes I-III as SA for INItiative  #225

---
+++ dev/CharacterModel/SpecialAbilities/CombatReflexesM.cs
@@ -0,0 +1,24 @@
+﻿using FateExplorer.Shared;
+
+namespace FateExplorer.CharacterModel.SpecialAbilities;
+
+[SpecialAbility(SA.CombatReflexes)]
+public class CombatReflexesM : TieredActivatableM
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Nicht verwendete Parameter entfernen", Justification = "Inheritance")]
+    public CombatReflexesM(string id, int tier, string[] reference, bool recognized) : base(id, tier, reference)
+    {
+        if (id != SA.CombatReflexes)
+            throw new ChrImportException("Given id is not combat reflexes", ChrImportException.Property.SpecialAbility);
+    }
+
+    /// <inheritdoc cref="IActivatableM.Apply(CharacterM)"/>
+    public override void Apply(CharacterM character)
+    {
+        character.Initiative.AddOnSetup(Tier * 1);
+    }
+
+    /// <inheritdoc/>
+    public override bool IsRecognized => true;
+}
+
