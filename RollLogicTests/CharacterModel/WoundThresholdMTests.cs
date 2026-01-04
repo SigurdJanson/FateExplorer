dcff8004630fa7c4e15f7b607674e3f03dfe2209
Jan <59577271+SigurdJanson@users.noreply.github.com>
2026-01-04 21:35:44 +0100

    New class for Wound Threshold values

---
+++ RollLogicTests/CharacterModel/WoundThresholdMTests.cs
@@ -0,0 +1,107 @@
+﻿using FateExplorer.CharacterModel;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.CharacterModel;
+
+[TestFixture]
+public class WoundThresholdMTests
+{
+    #region Test Helpers
+
+    private static Mock<ICharacterM> CreateHeroMock(int constitution)
+    {
+        var heroMock = new Mock<ICharacterM>();
+
+        heroMock
+            .Setup(h => h.GetAbility(AbilityM.CON))
+            .Returns(constitution);
+
+        return heroMock;
+    }
+
+    #endregion Test Helpers
+
+
+
+    #region Construction / Initial Value
+
+    [TestCase(10)] // sum is even
+    [TestCase(5)] // sum is odd
+    [TestCase(1)]
+    public void Constructor_ComputesCorrectInitialEffectiveValue(int constitution)
+    {
+        // Arrange
+        var hero = CreateHeroMock(constitution);
+
+        // Act
+        var woundthreshold = new WoundThresholdM(hero.Object);
+
+        // Assert
+        Assert.That(woundthreshold.Effective, Is.EqualTo(WoundThresholdM.ComputeValue(constitution)));
+    }
+
+    #endregion
+
+
+    #region Value Computation
+
+    [TestCase(14, ExpectedResult = 7)] //
+    [TestCase(15, ExpectedResult = 8)] //
+    [TestCase(12, ExpectedResult = 6)] // 
+    [TestCase(10, ExpectedResult = 5)] // 
+    public int ComputeValue_ComputesCorrectValue(int constitution)
+    {
+        // Arrange
+        // Act & Assert
+        return WoundThresholdM.ComputeValue(constitution);
+    }
+
+    #endregion
+
+
+    #region Dependency Updates
+
+    [TestCase(10, 12)]
+    [TestCase(8, 2)]
+    public void DependencyChange_CourageOnly_RecomputesUsingCachedAgility(
+        int initialConstitution,
+        int newConstitution)
+    {
+        // Arrange
+        var hero = CreateHeroMock(initialConstitution);
+        var woundthreshold = new WoundThresholdM(hero.Object);
+
+        // Act
+        woundthreshold.DependencyHasChanged(AbilityM.CON, newConstitution);
+
+        // Assert
+        Assert.That(
+            woundthreshold.Effective,
+            Is.EqualTo(WoundThresholdM.ComputeValue(newConstitution)));
+    }
+
+    #endregion
+
+
+
+    #region Ignored Dependencies
+
+    [Test]
+    public void DependencyChange_UnrelatedAttribute_DoesNotChangeEffectiveValue()
+    {
+        // Arrange
+        var hero = CreateHeroMock(6);
+        var woundThreshold = new WoundThresholdM(hero.Object);
+        var originalValue = woundThreshold.Effective;
+
+        // Act
+        woundThreshold.DependencyHasChanged("UNRELATED", 10);
+
+        // Assert
+        Assert.That(woundThreshold.Effective, Is.EqualTo(originalValue));
+    }
+
+    #endregion
+
+}
---
+++ dev/CharacterModel/WoundThresholdM.cs
@@ -0,0 +1,37 @@
+﻿namespace FateExplorer.CharacterModel;
+
+
+/// <summary>
+/// Represents the derived attribute "Wound Threshold"
+/// </summary>
+public class WoundThresholdM : DerivedValue
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="hero">The character of this dodge competence</param>
+    public WoundThresholdM(ICharacterM hero) : base(ComputeValue(hero.GetAbility(AbilityM.CON)))
+    {
+        //--Hero = hero;
+        Min = 0;
+        Max = 12;
+        DependencyId = [AbilityM.CON];
+    }
+
+    /// <summary>
+    /// Computes a valid wound threshold value from the given attributes
+    /// </summary>
+    /// <param name="EffectiveConstitution">The constitution of the character</param>
+    /// <returns>An effective initiative value</returns>
+    public static int ComputeValue(int EffectiveConstitution)
+        => (EffectiveConstitution + 1) / 2;
+
+
+    /// <inheritdoc/>
+    protected override void UpdateOnDependencyChange(string attrId, int newValue)
+    {
+        if (attrId != AbilityM.CON) return;
+        Effective = ComputeValue(newValue);
+    }
+
+}
